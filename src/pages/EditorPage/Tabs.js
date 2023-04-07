b5d0bd4e6309b38585d36489ff273821225b1127
marcinbodnar <me@marcinbodnar.com>
2023-04-07 22:37:23 +0200

    Change editors file structure

--- src/pages/EditorPage/Tabs.js
+++ src/pages/EditorPage/Tabs.js
@@ -1,23 +1,4 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
-import ls from "local-storage";
-import prettier from "prettier";
-import parserBabel from "prettier/parser-babel";
-import { useHistory, useParams } from "react-router-dom";
-import {
-  Widget,
-  useCache,
-  useNear,
-  CommitButton,
-  useAccountId,
-} from "near-social-vm";
-import { Nav, OverlayTrigger, Tooltip } from "react-bootstrap";
-import RenameModal from "../../../components/Editor/RenameModal";
-import OpenModal from "../../../components/Editor/OpenModal";
-import OpenModuleModal from "../../../components/Editor/OpenModuleModal";
-import AddModal from "../../../components/Editor/AddModal";
-import CreateModal from "../../../components/Editor/CreateModal";
-import { SaveDraftModal } from "../../../components/SaveDraft";
-import styled from "styled-components";
 
 export default function Tabs({
   isModule,
