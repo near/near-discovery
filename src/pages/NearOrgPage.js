1f577f2aa008a7494cfec2a62e28f663b81f1ff4
Caleb Jacob <thecalebjacob@gmail.com>
2023-04-13 11:38:08 -0600

    Remove test string

--- src/pages/NearOrgPage.js
+++ src/pages/NearOrgPage.js
@@ -39,7 +39,6 @@ export default function NearOrgPage(props) {
       <DesktopNavigation />
 
       <div>
-        <p>{props.src}</p>
         {(!localOverrideUrl || redirectMap) && (
           <Widget
             config={{ redirectMap: redirectMap }}
