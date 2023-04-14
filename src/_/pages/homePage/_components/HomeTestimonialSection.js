49cba9c29c870f023f1ad3a352f1e220d36de0d9
saidulbadhon <saidulbadhon@gmail.com>
2023-04-15 03:09:53 +0600

    Update HomeTestimonialSection.js

--- src/_/pages/homePage/_components/HomeTestimonialSection.js
+++ src/_/pages/homePage/_components/HomeTestimonialSection.js
@@ -2,21 +2,12 @@ import { Box } from "@mui/material";
 import React, { useContext } from "react";
 import { ThemeContext } from "../../../context/ThemeContext";
 
-import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import { Masonry } from "@mui/lab";
 
