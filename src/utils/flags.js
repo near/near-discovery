e54eb301c5652ab2c764f395985b22320669601a
Michael Peter <m.peter.dev@gmail.com>
2023-04-14 18:52:58 +0000

    flags page with support for bos loader url

---
+++ src/utils/flags.js
@@ -0,0 +1,24 @@
+import { useState, useCallback } from "react";
+
+export function useFlags() {
+  const [rawFlags, setRawFlags] = useState(
+    localStorage.getItem("flags")
+      ? JSON.parse(localStorage.getItem("flags"))
+      : {}
+  );
+
+  const setFlags = useCallback((newFlags) => {
+    setRawFlags((f) => {
+      const updated = { ...f, ...newFlags };
+      localStorage.setItem("flags", JSON.stringify(updated));
+
+      // reload for changes to take effect
+      location.reload();
+
+      // may not be reachable
+      return updated;
+    });
+  });
+
+  return [rawFlags, setFlags];
+}
