b56d5412b93bd9ac412a66a17d0c820506673900
marcinbodnar <me@marcinbodnar.com>
2023-04-16 22:21:43 +0200

    ForkButton refactor

--- src/pages/EditorPage/buttons/ForkButton.js
+++ src/pages/EditorPage/buttons/ForkButton.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { Filetype } from "../utils/const";
 
-export default function ForkButton({ widgetName, code, forkFile, toPath }) {
-  return (
-    <button className="btn btn-outline-primary me-2" onClick={() => forkFile()}>
-      Fork
-    </button>
-  );
-}
+export default ({ forkFile }) => (
+  <button className="btn btn-outline-primary me-2" onClick={forkFile}>
+    Fork
+  </button>
+);
