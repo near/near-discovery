3ccd1ce7ef5899eb919603c9fd1f2d3ebd66baf8
marcinbodnar <me@marcinbodnar.com>
2023-04-16 22:26:43 +0200

    SaveDraftButton refactor

--- src/pages/EditorPage/buttons/SaveDraftButton.js
+++ src/pages/EditorPage/buttons/SaveDraftButton.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { ModalTypes } from "../utils/const";
 
-export default function SaveDraftButton({ widgetName, setShowModal }) {
-  return (
-    <button
-      className="btn btn-outline-primary me-2"
-      disabled={!widgetName}
-      onClick={() => setShowModal(ModalTypes.SaveDraftModal)}
-    >
-      Save Version
-    </button>
-  );
-}
+export default ({ widgetName, setShowModal }) => (
+  <button
+    className="btn btn-outline-primary me-2"
+    disabled={!widgetName}
+    onClick={() => setShowModal(ModalTypes.SaveDraftModal)}
+  >
+    Save Version
+  </button>
+);
