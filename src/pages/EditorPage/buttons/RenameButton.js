3d0d63c2ab6e707b691ae12e8bae485bb5afc9d7
marcinbodnar <me@marcinbodnar.com>
2023-04-16 22:25:42 +0200

    RenameButton refactor

--- src/pages/EditorPage/buttons/RenameButton.js
+++ src/pages/EditorPage/buttons/RenameButton.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { ModalTypes } from "../utils/const";
 
-export default function RenameButton({ setShowRenameModal, setShowModal }) {
-  return (
-    <button
-      className="btn btn-outline-success ms-2"
-      style={{ height: "40px" }}
-      onClick={() => setShowModal(ModalTypes.RenameModal)}
-    >
-      <i className="bi bi-pen"></i>
-    </button>
-  );
-}
+export default ({ setShowModal }) => (
+  <button
+    className="btn btn-outline-success ms-2"
+    style={{ height: "40px" }}
+    onClick={() => setShowModal(ModalTypes.RenameModal)}
+  >
+    <i className="bi bi-pen"></i>
+  </button>
+);
