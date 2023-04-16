8d084aadd3bc81522072c3e9ba77b3ae58132506
marcinbodnar <me@marcinbodnar.com>
2023-04-16 22:24:53 +0200

    PublishButton refactor

--- src/pages/EditorPage/buttons/PublishButton.js
+++ src/pages/EditorPage/buttons/PublishButton.js
@@ -1,28 +1,20 @@
 import React from "react";
 import { CommitButton } from "near-social-vm";
 
-export default function PublishButton({
-  widgetName,
-  near,
-  path,
-  code,
-  metadata,
-}) {
-  return (
-    <CommitButton
-      className={`btn btn-primary`}
-      disabled={!widgetName}
-      near={near}
-      data={{
-        [path?.type]: {
-          [widgetName]: {
-            "": code,
-            metadata,
-          },
+export default ({ widgetName, near, path, code, metadata }) => (
+  <CommitButton
+    className={`btn btn-primary`}
+    disabled={!widgetName}
+    near={near}
+    data={{
+      [path?.type]: {
+        [widgetName]: {
+          "": code,
+          metadata,
         },
-      }}
-    >
-      Publish
-    </CommitButton>
-  );
-}
+      },
+    }}
+  >
+    Publish
+  </CommitButton>
+);
