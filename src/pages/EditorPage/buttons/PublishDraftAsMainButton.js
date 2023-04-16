cf14f92ffbf85542f7c6ff4b98d09f079d2f315d
marcinbodnar <me@marcinbodnar.com>
2023-04-16 22:25:17 +0200

    PublishDraftAsMainButton refactor

--- src/pages/EditorPage/buttons/PublishDraftAsMainButton.js
+++ src/pages/EditorPage/buttons/PublishDraftAsMainButton.js
@@ -1,31 +1,23 @@
 import React from "react";
 import { CommitButton } from "near-social-vm";
 
-export default function PublishDraftAsMainButton({
-  widgetName,
-  near,
-  path,
-  code,
-  metadata,
-}) {
-  return (
-    <CommitButton
-      className={`btn btn-primary`}
-      disabled={!widgetName}
-      near={near}
-      data={{
-        [path?.type]: {
-          [widgetName]: {
-            "": code,
-            metadata,
-            branch: {
-              draft: null,
-            },
+export default ({ widgetName, near, path, code, metadata }) => (
+  <CommitButton
+    className={`btn btn-primary`}
+    disabled={!widgetName}
+    near={near}
+    data={{
+      [path?.type]: {
+        [widgetName]: {
+          "": code,
+          metadata,
+          branch: {
+            draft: null,
           },
         },
-      }}
-    >
-      Publish
-    </CommitButton>
-  );
-}
+      },
+    }}
+  >
+    Publish
+  </CommitButton>
+);
