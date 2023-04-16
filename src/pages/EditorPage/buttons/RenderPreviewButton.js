e7f5b867baa50db3b74fd6e7b70702fb91f6758a
marcinbodnar <me@marcinbodnar.com>
2023-04-16 22:26:24 +0200

    RenderPreviewButton refactor

--- src/pages/EditorPage/buttons/RenderPreviewButton.js
+++ src/pages/EditorPage/buttons/RenderPreviewButton.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Layout, Tab } from "../utils/const";
 
-export default function RenderPreviewButton({ setRenderCode, layout, setTab }) {
-  return (
-    <button
-      className="btn btn-outline-primary"
-      onClick={() => {
-        setRenderCode(code);
-        if (layout === Layout.Tabs) {
-          setTab(Tab.Widget);
-        }
-      }}
-    >
-      Render Preview
-    </button>
-  );
-}
+export default ({ setRenderCode, layout, setTab }) => (
+  <button
+    className="btn btn-outline-primary"
+    onClick={() => {
+      setRenderCode(code);
+      if (layout === Layout.Tabs) {
+        setTab(Tab.Widget);
+      }
+    }}
+  >
+    Render Preview
+  </button>
+);
