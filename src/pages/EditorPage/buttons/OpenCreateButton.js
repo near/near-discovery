bde02fdccf8a97a9930cc1454b9f819d52edf499
marcinbodnar <me@marcinbodnar.com>
2023-04-16 22:23:23 +0200

    OpenCreateButton refactor

--- src/pages/EditorPage/buttons/OpenCreateButton.js
+++ src/pages/EditorPage/buttons/OpenCreateButton.js
@@ -1,20 +1,18 @@
 import React from "react";
 import { ModalTypes } from "../utils/const";
 
-export default function OpenCreateButton({ setShowModal }) {
-  return (
-    <button
-      className="btn btn-success ms-2"
-      onClick={() => setShowModal(ModalTypes.AddModal)}
-      style={{
-        fontSize: "20px",
-        height: "40px",
-        lineHeight: "38px",
-        paddingTop: "0",
-        marginTop: "0",
-      }}
-    >
-      <i className="bi bi-plus"></i>
-    </button>
-  );
-}
+export default ({ setShowModal }) => (
+  <button
+    className="btn btn-success ms-2"
+    onClick={() => setShowModal(ModalTypes.AddModal)}
+    style={{
+      fontSize: "20px",
+      height: "40px",
+      lineHeight: "38px",
+      paddingTop: "0",
+      marginTop: "0",
+    }}
+  >
+    <i className="bi bi-plus"></i>
+  </button>
+);
