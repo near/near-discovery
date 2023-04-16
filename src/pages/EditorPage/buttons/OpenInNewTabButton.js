f4a322e2fbdbc2515423855f30398911ab059b02
marcinbodnar <me@marcinbodnar.com>
2023-04-16 22:24:20 +0200

    OpenInNewTabButton refactor

--- src/pages/EditorPage/buttons/OpenInNewTabButton.js
+++ src/pages/EditorPage/buttons/OpenInNewTabButton.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-export default function OpenInNewTabButton({ widgetPath }) {
-  return (
-    <a
-      className="btn me-2 btn-outline-secondary"
-      style={{ height: "38px" }}
-      href={`#/${widgetPath}`}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Open Component
-    </a>
-  );
-}
+export default ({ widgetPath }) => (
+  <a
+    className="btn me-2 btn-outline-secondary"
+    style={{ height: "38px" }}
+    href={`#/${widgetPath}`}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Open Component
+  </a>
+);
