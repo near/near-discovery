b5cc5affdc233f72f40b7d9ead0933d81106b19c
Dorian Kinoo Crutcher <doriankinoocrutcher@gmail.com>
2023-04-18 20:31:54 -0700

    removed unused custom stylings

--- src/components/navigation/alpha/desktop/SearchDropDown.js
+++ src/components/navigation/alpha/desktop/SearchDropDown.js
@@ -4,70 +4,12 @@ import { Widget } from "near-social-vm";
 import styled from "styled-components";
 
 const SearchDropDown = (props) => {
-  const typeAheadContainer = {
-    width: "513px",
-    height: "458px",
-    zIndex: "3",
-    backgroundColor: "black",
-    borderRadius: "10px",
-    transform: "translateX(50px)",
-    display: "flex",
-    flexDirection: "column",
-    textAlign: "center",
-    justifyContent: "center",
-    alignItems: "center",
-  };
-
-  console.log("searchValue is", props.searchValue);
-
   // Update the Footer styling
-  const Footer = styled.div`
-    display: flex;
-    justify-content: right;
-    padding: 24px 0;
-    border-top: 1px solid rgba(96, 109, 122, 0.4);
-  `;
 
-  const Button = styled.button`
-    background-color: transparent;
-    border: none;
-    font-size: 14px;
-    font-weight: 600;
-    color: #9799f8;
-    cursor: pointer;
-    padding: 0;
-    text-decoration: underline;
-
-    &:hover {
-      color: #9799f8;
-    }
-  `;
-
-  const MainContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100vh; // This ensures that the container takes up the full viewport height
-  `;
-  const ContentContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-  `;
-
-  const ScrollableContent = styled.div`
-    overflow-y: auto;
-    flex-grow: 1;
-  `;
-
-  const FixedFooter = styled.footer`
-    padding: 1rem;
-    text-align: right;
-    border-top: 1px solid rgba(96, 109, 122, 0.4);
-  `;
+  const ContentContainer = styled.div``;
 
   return (
     <div
-      style={typeAheadContainer}
       onFocus={() => props.focusChange(true)}
       onBlur={() => props.focusChange(false)}
       onClick={async () => {
