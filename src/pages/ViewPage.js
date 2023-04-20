411c149c1b9fb46845c76e624c025d425e45baf3
Dmitriy <sheleg.dmitriy@gmail.com>
2023-04-20 16:24:21 +0300

    Specify meta tags for each page

--- src/pages/ViewPage.js
+++ src/pages/ViewPage.js
@@ -5,6 +5,7 @@ import { useQuery } from "../hooks/useQuery";
 import { useHashUrlBackwardsCompatibility } from "../hooks/useHashUrlBackwardsCompatibility";
 import styleZendesk from "../zendesk";
 import util from "util";
+import { Helmet } from "react-helmet";
 
 export default function ViewPage(props) {
   // will always be empty in prod
@@ -99,78 +100,86 @@ export default function ViewPage(props) {
   useEffect(queueZendeskCheck, []);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      {localOverrideUrl && (
-        <div
-          style={{
-            backgroundColor: "#FFF2CD",
-            color: "#664D04",
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "2rem",
-            columnGap: "8px",
-          }}
-        >
-          <span>Loading components from: {localOverrideUrl}</span>
-          {props.flags?.bosLoaderUrl && (
-            <button
-              className="btn btn-outline"
-              onClick={() => props.setFlags?.({ bosLoaderUrl: null })}
-            >
-              <i className="bi bi-x" />
-            </button>
-          )}
-        </div>
-      )}
-      {loaderError && (
-        <div style={{ padding: "16px" }}>
-          BOS Loader fetch error, see console logs. CORS errors may be
-          misleading and mean your endpoint cannot be reached
-        </div>
-      )}
-      <div className="container-xl">
-        <div className="row">
+    <>
+      <Helmet>
+        <title>{props.meta.title}</title>
+        <meta name="description" content={props.meta.description} />
+        <meta property="og:title" content={props.meta.title} />
+        <meta property="og:description" content={props.meta.description} />
+      </Helmet>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {localOverrideUrl && (
           <div
-            className="d-inline-block position-relative overflow-hidden"
             style={{
-              "--body-top-padding": "24px",
-              paddingTop: "var(--body-top-padding)",
+              backgroundColor: "#FFF2CD",
+              color: "#664D04",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "2rem",
+              columnGap: "8px",
             }}
           >
-            {(!localOverrideUrl || redirectMap) && (
-              <div>
-                <Widget
-                  config={{ redirectMap: redirectMap }}
-                  key={props.widgets.wrapper}
-                  src={props.widgets.wrapper}
-                  props={{
-                    children: (
-                      <Widget
-                        config={{ redirectMap: redirectMap }}
-                        key={props.tos.checkComponentPath}
-                        src={props.tos.checkComponentPath}
-                        props={{
-                          logOut: props.logOut,
-                          targetProps: widgetProps,
-                          targetComponent: src,
-                          tosName: props.tos.contentComponentPath,
-                        }}
-                      />
-                    ),
-                  }}
-                />
-              </div>
+            <span>Loading components from: {localOverrideUrl}</span>
+            {props.flags?.bosLoaderUrl && (
+              <button
+                className="btn btn-outline"
+                onClick={() => props.setFlags?.({ bosLoaderUrl: null })}
+              >
+                <i className="bi bi-x" />
+              </button>
             )}
+          </div>
+        )}
+        {loaderError && (
+          <div style={{ padding: "16px" }}>
+            BOS Loader fetch error, see console logs. CORS errors may be
+            misleading and mean your endpoint cannot be reached
+          </div>
+        )}
+        <div className="container-xl">
+          <div className="row">
+            <div
+              className="d-inline-block position-relative overflow-hidden"
+              style={{
+                "--body-top-padding": "24px",
+                paddingTop: "var(--body-top-padding)",
+              }}
+            >
+              {(!localOverrideUrl || redirectMap) && (
+                <div>
+                  <Widget
+                    config={{ redirectMap: redirectMap }}
+                    key={props.widgets.wrapper}
+                    src={props.widgets.wrapper}
+                    props={{
+                      children: (
+                        <Widget
+                          config={{ redirectMap: redirectMap }}
+                          key={props.tos.checkComponentPath}
+                          src={props.tos.checkComponentPath}
+                          props={{
+                            logOut: props.logOut,
+                            targetProps: widgetProps,
+                            targetComponent: src,
+                            tosName: props.tos.contentComponentPath,
+                          }}
+                        />
+                      ),
+                    }}
+                  />
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
