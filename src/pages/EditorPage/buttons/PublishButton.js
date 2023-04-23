285a99294e10d984e8688f929d1a98e67f440a80
marcinbodnar <me@marcinbodnar.com>
2023-04-23 23:23:15 +0200

    Fix drafts

--- src/pages/EditorPage/buttons/PublishButton.js
+++ src/pages/EditorPage/buttons/PublishButton.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { CommitButton } from "near-social-vm";
 
-export default ({ widgetName, near, path, codeVisible, metadata, disable }) => {
+export default ({
+  widgetName,
+  near,
+  path,
+  codeVisible,
+  metadata,
+  disable,
+  handleCommit,
+}) => {
   return (
     <div>
       <CommitButton
         className={`btn btn-primary`}
         disabled={!widgetName || disable.publishButton}
         near={near}
+        onCommit={handleCommit}
         data={{
           [path?.type]: {
             [widgetName]: {
