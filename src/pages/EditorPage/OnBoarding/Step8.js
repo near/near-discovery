3917d21847302df127c2b2007a5f4f009daaa172
marcinbodnar <me@marcinbodnar.com>
2023-04-24 19:07:15 +0200

    Update copy

--- src/pages/EditorPage/OnBoarding/Step8.js
+++ src/pages/EditorPage/OnBoarding/Step8.js
@@ -2,7 +2,10 @@ import React from "react";
 
 export default () => (
   <div>
-    <div className="title">Replace the code</div>
-    <div className="desc">Replace the placeholder code on line 6.</div>
+    <div className="title">Paste the embed code and preview your changes</div>
+    <div className="desc">
+      Paste the embedded code on line 6 and review the changes by clicking
+      “Render Preview”
+    </div>
   </div>
 );
