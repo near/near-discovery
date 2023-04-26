a770990338175c4bb5201935d590f25f25322ee1
Michael Peter <m.peter.dev@gmail.com>
2023-04-26 17:48:00 +0000

    prettier

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -1,11 +1,11 @@
-import { ACCOUNT_ID_SUFFIX, handleCreateAccount } from '../utils/auth';
-import { getEmailId, isValidEmail, parseURLParams } from '../utils/generic';
+import { ACCOUNT_ID_SUFFIX, handleCreateAccount } from "../utils/auth";
+import { getEmailId, isValidEmail, parseURLParams } from "../utils/generic";
 
-import React from 'react';
-import styled from 'styled-components';
-import { toast } from 'sonner'
-import { useForm } from 'react-hook-form';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
 
 const CreateAccount = () => {
   const history = useHistory();
@@ -16,128 +16,152 @@ const CreateAccount = () => {
 
   const checkIsAccountAvailable = async () => {
     try {
-      if (!formValues?.username) return
+      if (!formValues?.username) return;
 
-      const response = await fetch('https://rpc.testnet.near.org', {
-        method: 'POST',
+      const response = await fetch("https://rpc.testnet.near.org", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          "jsonrpc": "2.0",
-          "id": "dontcare",
-          "method": "query",
-          "params": {
-            "request_type": "view_account",
-            "finality": "final",
-            "account_id": `${formValues?.username}.${ACCOUNT_ID_SUFFIX}`
-          }
-        })
+          jsonrpc: "2.0",
+          id: "dontcare",
+          method: "query",
+          params: {
+            request_type: "view_account",
+            finality: "final",
+            account_id: `${formValues?.username}.${ACCOUNT_ID_SUFFIX}`,
+          },
+        }),
       });
       const data = await response.json();
       if (data?.error?.cause?.name == "UNKNOWN_ACCOUNT") {
-        return setIsAccountAvailable(true)
+        return setIsAccountAvailable(true);
       }
 
       if (data?.result?.code_hash) {
-        return setIsAccountAvailable(false)
+        return setIsAccountAvailable(false);
       }
     } catch (error) {
-      console.log(error)
-      setIsAccountAvailable(false)
+      console.log(error);
+      setIsAccountAvailable(false);
     }
-  }
-
+  };
 
   const onSubmit = handleSubmit(async (data) => {
-    if (!data?.username || !data.email) return
+    if (!data?.username || !data.email) return;
     try {
-      const fullAccountId = `${data.username}.${ACCOUNT_ID_SUFFIX}`
-      const { publicKey, accountId, email } = await handleCreateAccount(fullAccountId, data.email)
-      history.push(`/verify-email?publicKey=${encodeURIComponent(publicKey)}&accountId=${encodeURIComponent(accountId)}&email=${encodeURIComponent(email)}`)
+      const fullAccountId = `${data.username}.${ACCOUNT_ID_SUFFIX}`;
+      const { publicKey, accountId, email } = await handleCreateAccount(
+        fullAccountId,
+        data.email
+      );
+      history.push(
+        `/verify-email?publicKey=${encodeURIComponent(
+          publicKey
+        )}&accountId=${encodeURIComponent(
+          accountId
+        )}&email=${encodeURIComponent(email)}`
+      );
     } catch (error) {
-      toast.error(error.message)
+      toast.error(error.message);
     }
   });
 
+  React.useEffect(() => {
+    if (!formValues?.username) return;
+    checkIsAccountAvailable();
+  }, [formValues?.username]);
 
   React.useEffect(() => {
-    if (!formValues?.username) return
-    checkIsAccountAvailable()
-  }, [formValues?.username])
-
-  React.useEffect(() => {
-    const params = parseURLParams(window.location.search)
-    setUrlParams(params)
-  }, [window.location.search])
+    const params = parseURLParams(window.location.search);
+    setUrlParams(params);
+  }, [window.location.search]);
 
   return (
     <StyledContainer>
       <FormContainer onSubmit={onSubmit}>
         <header>
           <h1>Create account</h1>
-          <p className='desc'>Use this account to sign in everywhere on NEAR, no password required.</p>
+          <p className="desc">
+            Use this account to sign in everywhere on NEAR, no password
+            required.
+          </p>
         </header>
 
         <InputContainer>
           <label htmlFor="email">Email</label>
 
           <input
-            {...register('email', {
-              required: 'Please enter a valid email address',
+            {...register("email", {
+              required: "Please enter a valid email address",
             })}
             onChange={(e) => {
-              setValue('email', e.target.value)
-              if (!isValidEmail(e.target.value)) return
+              setValue("email", e.target.value);
+              if (!isValidEmail(e.target.value)) return;
               if (!formValues?.username || formValues.username === username) {
-                setValue('username', getEmailId(e.target.value))
+                setValue("username", getEmailId(e.target.value));
               }
             }}
-            label='Email'
-            placeholder='user_name@email.com'
+            label="Email"
+            placeholder="user_name@email.com"
             type="email"
             required
           />
         </InputContainer>
 
-
         <InputContainer>
           <label htmlFor="username">Account ID</label>
           <input
-            autoComplete='webauthn username'
-            {...register('username', {
-              required: 'Please enter a valid account ID',
+            autoComplete="webauthn username"
+            {...register("username", {
+              required: "Please enter a valid account ID",
             })}
             onChange={(e) => {
-              setValue('username', e.target.value)
+              setValue("username", e.target.value);
 
               if (e.target.value == "") {
-                setIsAccountAvailable(null)
+                setIsAccountAvailable(null);
               }
             }}
-            label='Account ID'
-            placeholder='user_name.near'
-            statusState={isAccountAvailable === null ? 'default' : !!isAccountAvailable ? 'success' : 'error'}
-            statusMessage={isAccountAvailable === null ? 'Use a suggested ID or customize your own.' : !!isAccountAvailable ? `${formValues?.username}.${ACCOUNT_ID_SUFFIX} is available!` : `${formValues?.accountId}.${ACCOUNT_ID_SUFFIX} is taken, try something else.`}
+            label="Account ID"
+            placeholder="user_name.near"
+            statusState={
+              isAccountAvailable === null
+                ? "default"
+                : !!isAccountAvailable
+                ? "success"
+                : "error"
+            }
+            statusMessage={
+              isAccountAvailable === null
+                ? "Use a suggested ID or customize your own."
+                : !!isAccountAvailable
+                ? `${formValues?.username}.${ACCOUNT_ID_SUFFIX} is available!`
+                : `${formValues?.accountId}.${ACCOUNT_ID_SUFFIX} is taken, try something else.`
+            }
           />
           <p className="subText">
-            {isAccountAvailable === null ? 'Use a suggested ID or customize your own.' : !!isAccountAvailable ? `${formValues?.username}.${ACCOUNT_ID_SUFFIX} is available!` : `${formValues?.username}.${ACCOUNT_ID_SUFFIX} is taken, try something else.`}
+            {isAccountAvailable === null
+              ? "Use a suggested ID or customize your own."
+              : !!isAccountAvailable
+              ? `${formValues?.username}.${ACCOUNT_ID_SUFFIX} is available!`
+              : `${formValues?.username}.${ACCOUNT_ID_SUFFIX} is taken, try something else.`}
           </p>
-
         </InputContainer>
         <StyledButton fullWidth onClick={onSubmit} type="button">
           Continue
         </StyledButton>
         <Footer>
-          By creating an account, you agree to the NEAR <a href="">terms of service</a> and <a href="">privacy policy</a>
+          By creating an account, you agree to the NEAR{" "}
+          <a href="">terms of service</a> and <a href="">privacy policy</a>
         </Footer>
       </FormContainer>
     </StyledContainer>
-  )
-}
+  );
+};
 
-export default CreateAccount
-
+export default CreateAccount;
 
 const StyledContainer = styled.div`
   width: 100%;
@@ -145,21 +169,21 @@ const StyledContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: #F2F1EA;
+  background-color: #f2f1ea;
   padding: 0 16px;
-`
+`;
 
 const FormContainer = styled.form`
   max-width: 450px;
   width: 100%;
   margin: 16px auto;
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   padding: 16px;
   border-radius: 12px;
   display: flex;
   flex-direction: column;
   gap: 16px;
-`
+`;
 
 const InputContainer = styled.div`
   width: 100%;
@@ -173,24 +197,23 @@ const InputContainer = styled.div`
 
   input {
     padding: 8px 12px;
-    border: 1px solid #E5E5E5;
+    border: 1px solid #e5e5e5;
     border-radius: 10px;
     font-size: 14px;
     margin-top: 4px;
-    min-height: 50px; 
+    min-height: 50px;
     cursor: text;
-
 
     &:focus {
       outline: none;
-      border: 1px solid #E5E5E5;
+      border: 1px solid #e5e5e5;
     }
   }
 
   .subText {
     font-size: 12px;
   }
-`
+`;
 
 const StyledButton = styled.button`
   padding: 8px;
@@ -200,7 +223,7 @@ const StyledButton = styled.button`
   margin-top: 4px;
   min-height: 40px;
   cursor: pointer;
-  background-color: #6BE89E;
+  background-color: #6be89e;
   color: #000000;
   font-weight: 500;
   display: flex;
@@ -211,7 +234,7 @@ const StyledButton = styled.button`
   &:focus {
     outline: none;
   }
-`
+`;
 
 const Footer = styled.div`
   padding: 12px 16px;
@@ -220,7 +243,7 @@ const Footer = styled.div`
   justify-self: flex-end;
   align-self: flex-end;
 
-  'a': {
-      color: '#37819F'
+  "a": {
+    color: "#37819F";
   }
-`
+`;
