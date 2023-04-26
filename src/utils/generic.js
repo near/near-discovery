8a839d777ec2297d92b4c825c5feb2e8a4c7ca1f
Michael Peter <m.peter.dev@gmail.com>
2023-04-26 17:48:00 +0000

    prettier

--- src/utils/generic.js
+++ src/utils/generic.js
@@ -1,17 +1,17 @@
 export const getEmailId = (email) => {
-    return email.split('@')[0];
-}
+  return email.split("@")[0];
+};
 
 export const isValidEmail = (email) => {
-    const re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+};
 
 export const parseURLParams = (url) => {
-    const params = new URLSearchParams(url);
-    const data = {};
-    for (const [key, value] of params) {
-        data[key] = value;
-    }
-    return data;
-}
+  const params = new URLSearchParams(url);
+  const data = {};
+  for (const [key, value] of params) {
+    data[key] = value;
+  }
+  return data;
+};
