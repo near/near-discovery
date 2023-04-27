3d02503c0509578bfb57680889b8aece000cf68c
gutsyphilip <philip.c.obosi@gmail.com>
2023-04-27 23:09:29 +0400

    fix: dynamic rpc url

--- src/data/widgets.js
+++ src/data/widgets.js
@@ -7,6 +7,28 @@ const TestnetDomains = {
 export const NetworkId =
   window.location.hostname in TestnetDomains ? "testnet" : "mainnet";
 
+export const networks = {
+  mainnet: {
+    networkId: 'mainnet',
+    viewAccountId: 'near',
+    nodeUrl: 'https://rpc.mainnet.near.org',
+    walletUrl: 'https://wallet.near.org',
+    helperUrl: 'https://helper.mainnet.near.org'
+  },
+  testnet: {
+    networkId: 'testnet',
+    viewAccountId: 'testnet',
+    nodeUrl: 'https://rpc.testnet.near.org',
+    walletUrl: 'https://wallet.testnet.near.org',
+    helperUrl: 'https://helper.testnet.near.org'
+  },
+  localnet: {
+    networkId: 'localnet',
+    viewAccountId: 'test.near',
+  }
+}
+
+
 const TestnetWidgets = {
   image: "eugenethedream/widget/Image",
   default: "one.testnet/widget/ActivityPage",
