0c7165e2ec1cb831e3014d67c0fc622e2abca76a
Caleb Jacob <thecalebjacob@gmail.com>
2023-05-01 15:46:41 -0600

    Flags hook, moving assets, setting up global css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,54 +1,38 @@
-import "@/styles/globals.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "@near-wallet-selector/modal-ui/styles.css";
-import "bootstrap/dist/js/bootstrap.bundle";
-import "App.scss";
+import '@/styles/globals.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '@near-wallet-selector/modal-ui/styles.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
-import type { AppProps } from "next/app";
+import { setupWalletSelector } from '@near-wallet-selector/core';
+import { setupHereWallet } from '@near-wallet-selector/here-wallet';
+import { setupMeteorWallet } from '@near-wallet-selector/meteor-wallet';
+import type { WalletSelectorModal } from '@near-wallet-selector/modal-ui';
+import { setupModal } from '@near-wallet-selector/modal-ui';
+import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
+import { setupNearWallet } from '@near-wallet-selector/near-wallet';
+import { setupNeth } from '@near-wallet-selector/neth';
+import { setupSender } from '@near-wallet-selector/sender';
+import Big from 'big.js';
+import { setupKeypom } from 'keypom-js';
+import { EthersProviderContext, useAccount, useInitNear, useNear, utils } from 'near-social-vm';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Toaster } from 'sonner';
 
-import { NetworkId, signInContractId, Widgets } from "@/data/widgets";
-import React, { useCallback, useEffect, useState } from "react";
-
-import Big from "big.js";
-import { Toaster } from "sonner";
-import { setupFastAuth } from "@/lib/selector/setup";
-import { setupHereWallet } from "@near-wallet-selector/here-wallet";
-import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
-import {
-  setupModal,
-  WalletSelectorModal,
-} from "@near-wallet-selector/modal-ui";
-import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
-import { setupNearWallet } from "@near-wallet-selector/near-wallet";
-import { setupNeth } from "@near-wallet-selector/neth";
-import { setupSender } from "@near-wallet-selector/sender";
-import { setupWalletSelector } from "@near-wallet-selector/core";
-import {
-  useAccount,
-  useInitNear,
-  useNear,
-  utils,
-  EthersProviderContext,
-} from "near-social-vm";
-import { useEthersProviderContext } from "@/data/web3";
-import {
-  init as initializeSegment,
-  recordWalletConnect,
-  reset as resetSegment,
-} from "@/utils/analytics";
-import { setupKeypom } from "keypom-js";
-import { KEYPOM_OPTIONS } from "@/utils/keypom-options";
-import { useRouter } from "next/router";
-import { useAuthStore } from "@/stores/auth";
+import { useEthersProviderContext } from '@/data/web3';
+import { NetworkId, signInContractId } from '@/data/widgets';
+import { setupFastAuth } from '@/lib/selector/setup';
+import { useAuthStore } from '@/stores/auth';
+import { init as initializeSegment, recordWalletConnect, reset as resetSegment } from '@/utils/analytics';
+import { KEYPOM_OPTIONS } from '@/utils/keypom-options';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [signedIn, setSignedIn] = useState(false);
   const [signedAccountId, setSignedAccountId] = useState(null);
   const [availableStorage, setAvailableStorage] = useState<Big | null>(null);
-  const [walletModal, setWalletModal] = useState<WalletSelectorModal | null>(
-    null
-  );
-  const [widgetSrc, setWidgetSrc] = useState(null);
+  const [walletModal, setWalletModal] = useState<WalletSelectorModal | null>(null);
   const ethersProviderContext = useEthersProviderContext();
   const router = useRouter();
   const { initNear } = useInitNear();
@@ -73,25 +57,23 @@ export default function App({ Component, pageProps }: AppProps) {
             setupHereWallet(),
             setupMeteorWallet(),
             setupNeth({
-              gas: "300000000000000",
+              gas: '300000000000000',
               bundle: false,
             }),
             setupFastAuth({
               networkId: NetworkId,
               signInContractId,
               relayerUrl:
-                NetworkId === "testnet"
-                  ? "http://34.70.226.83:3030/relay"
-                  : "https://near-relayer-mainnet.api.pagoda.co/relay",
+                NetworkId === 'testnet'
+                  ? 'http://34.70.226.83:3030/relay'
+                  : 'https://near-relayer-mainnet.api.pagoda.co/relay',
               hideModal: undefined, // TODO: Remove?
             }) as any, // TODO: Refactor setupFastAuth() to TS
             setupKeypom({
               trialBaseUrl:
-                NetworkId == "testnet"
-                  ? "https://test.near.org/#trial-url/"
-                  : "https://near.org/#trial-url/",
+                NetworkId == 'testnet' ? 'https://test.near.org/#trial-url/' : 'https://near.org/#trial-url/',
               networkId: NetworkId,
-              trialSplitDelim: "/",
+              trialSplitDelim: '/',
               signInContractId,
               modalOptions: KEYPOM_OPTIONS(NetworkId),
             }) as any, // TODO: Refactor setupKeypom() to TS
@@ -105,9 +87,7 @@ export default function App({ Component, pageProps }: AppProps) {
       return;
     }
     near.selector.then((selector: any) => {
-      setWalletModal(
-        setupModal(selector, { contractId: near.config.contractName })
-      );
+      setWalletModal(setupModal(selector, { contractId: near.config.contractName }));
     });
   }, [near]);
 
@@ -117,11 +97,11 @@ export default function App({ Component, pageProps }: AppProps) {
       walletModal?.show();
       return false;
     },
-    [walletModal]
+    [walletModal],
   );
 
   const requestSignIn = useCallback(() => {
-    router.push("/signin");
+    router.push('/signin');
   }, [router]);
 
   const logOut = useCallback(async () => {
@@ -134,13 +114,11 @@ export default function App({ Component, pageProps }: AppProps) {
     setSignedIn(false);
     setSignedAccountId(null);
     resetSegment();
-    localStorage.removeItem("accountId");
+    localStorage.removeItem('accountId');
   }, [near]);
 
   const refreshAllowance = useCallback(async () => {
-    alert(
-      "You're out of access key allowance. Need sign in again to refresh it"
-    );
+    alert("You're out of access key allowance. Need sign in again to refresh it");
     await logOut();
     requestSignIn();
   }, [logOut, requestSignIn]);
@@ -155,15 +133,13 @@ export default function App({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
     setAvailableStorage(
-      account.storageBalance
-        ? Big(account.storageBalance.available).div(utils.StorageCostPerByte)
-        : Big(0)
+      account.storageBalance ? Big(account.storageBalance.available).div(utils.StorageCostPerByte) : Big(0),
     );
   }, [account]);
 
   useEffect(() => {
-    if (navigator.userAgent !== "ReactSnap") {
-      const pageFlashPrevent = document.getElementById("page-flash-prevent");
+    if (navigator.userAgent !== 'ReactSnap') {
+      const pageFlashPrevent = document.getElementById('page-flash-prevent');
       if (pageFlashPrevent) {
         pageFlashPrevent.remove();
       }
@@ -191,11 +167,27 @@ export default function App({ Component, pageProps }: AppProps) {
     updateAuthStore,
   ]);
 
-  return (
-    <EthersProviderContext.Provider value={ethersProviderContext}>
-      <Component {...pageProps} />
+  const metaProps = {
+    title: 'NEAR',
+    description: "Let's build decentralized experiences.",
+  };
 
-      <Toaster position="bottom-center" richColors />
-    </EthersProviderContext.Provider>
+  return (
+    <>
+      <Head>
+        <title>{metaProps.title}</title>
+        <meta property="og:title" content={metaProps.title} />
+        <meta name="description" content={metaProps.description} />
+        <meta property="og:description" content={metaProps.description} />
+
+        <script src="https://unpkg.com/@phosphor-icons/web@2.0.3" async></script>
+      </Head>
+
+      <EthersProviderContext.Provider value={ethersProviderContext}>
+        <Component {...pageProps} />
+
+        <Toaster position="bottom-center" richColors />
+      </EthersProviderContext.Provider>
+    </>
   );
 }
