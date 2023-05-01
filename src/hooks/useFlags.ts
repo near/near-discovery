14ca131a55ddffd618a916d2e90ff1c83adbe6a4
Caleb Jacob <thecalebjacob@gmail.com>
2023-05-01 15:46:41 -0600

    Flags hook, moving assets, setting up global css

---
+++ src/hooks/useFlags.ts
@@ -0,0 +1,36 @@
+import { useState, useCallback } from 'react';
+
+/**
+ * Use Application flags
+ *
+ * `const [flags, setFlags] = useFlags();`
+ *
+ * Warning: setFlags causes page reload
+ */
+
+type Flags = {
+  bosLoaderUrl?: string;
+};
+
+export function useFlags() {
+  const [rawFlags, setRawFlags] = useState<Flags>(
+    localStorage.getItem('flags') ? JSON.parse(localStorage.getItem('flags') || '') : {},
+  );
+
+  const setFlags = useCallback((newFlags: Flags) => {
+    setRawFlags((f) => {
+      const updated = { ...f, ...newFlags };
+      localStorage.setItem('flags', JSON.stringify(updated));
+
+      alert('Saved flags');
+
+      // reload for changes to take effect
+      location.reload();
+
+      // may not be reachable
+      return updated;
+    });
+  }, []);
+
+  return [rawFlags, setFlags] as const;
+}
---
+++ src/pages/flags.tsx
@@ -0,0 +1,58 @@
+import type { MouseEvent } from 'react';
+import React, { useCallback, useState } from 'react';
+
+import { useFlags } from '@/hooks/useFlags';
+
+export default function FlagsPage() {
+  const [flags, setFlags] = useFlags();
+  const [bosLoaderUrl, setBosLoaderUrl] = useState(flags?.bosLoaderUrl || '');
+
+  const handleSave = useCallback(
+    (e: MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault();
+      setFlags({
+        bosLoaderUrl,
+      });
+    },
+    [bosLoaderUrl, setFlags],
+  );
+
+  return (
+    <>
+      <form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          rowGap: '1rem',
+          padding: '40px',
+        }}
+      >
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr',
+          }}
+        >
+          <div style={{ verticalAlign: 'middle' }}>BOS Loader Url</div>
+          <input
+            className="form-control"
+            placeholder="e.g. http://127.0.0.1:3030/, https://my-loader.ngrok.io"
+            value={bosLoaderUrl}
+            onChange={(e) => setBosLoaderUrl(e.target.value)}
+          />
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <button className="btn btn-primary" style={{ width: '10rem' }} onClick={handleSave}>
+            Save
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
