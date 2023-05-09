58f1b2544a75c2b0853ea8628051c57e57fbce9e
Caleb Jacob <thecalebjacob@gmail.com>
2023-05-09 13:28:00 -0600

    Adding comment to all BOS JSX near org pages

--- src/components/near-org/NearOrg.Ecosystem.GetFundingPage.jsx
+++ src/components/near-org/NearOrg.Ecosystem.GetFundingPage.jsx
@@ -1,3 +1,5 @@
+// NOTE: This page is a temporary hack to solve SEO. Don't convert to TSX - leave as JSX.
+
 import styled from 'styled-components';
 
 import { VmComponent } from '@/components/client/VmComponent';
