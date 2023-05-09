9d7d382c1458ac46544d3b29425a58d5bac81c2b
Caleb Jacob <thecalebjacob@gmail.com>
2023-05-09 13:33:10 -0600

    Refactor useScrollBlock to TS

--- src/hooks/useScrollBlock.ts
+++ src/hooks/useScrollBlock.ts
@@ -3,14 +3,14 @@
  * const [blockScroll, allowScroll] = useScrollBlock();
  */
 export function useScrollBlock() {
-  const safeDocument = typeof document !== 'undefined' ? document : {};
-  const html = safeDocument.documentElement;
-  const { body } = safeDocument;
-
   const blockScroll = () => {
+    if (typeof document === 'undefined') return;
+
+    const { body, documentElement } = document;
+
     if (!body || !body.style) return;
 
-    const scrollBarWidth = window.innerWidth - html.clientWidth;
+    const scrollBarWidth = window.innerWidth - documentElement.clientWidth;
     const bodyPaddingRight = parseInt(window.getComputedStyle(body).getPropertyValue('padding-right')) || 0;
 
     /**
@@ -19,18 +19,20 @@ export function useScrollBlock() {
      * 2. Fixes a bug in desktop Safari where `overflowY` does not prevent
      *    scroll if an `overflow-x` style is also applied to the body.
      */
-    html.style.position = 'relative'; /* [1] */
-    html.style.overflow = 'hidden'; /* [2] */
+    documentElement.style.position = 'relative'; /* [1] */
+    documentElement.style.overflow = 'hidden'; /* [2] */
     body.style.position = 'relative'; /* [1] */
     body.style.overflow = 'hidden'; /* [2] */
     body.style.paddingRight = `${bodyPaddingRight + scrollBarWidth}px`;
   };
 
   const allowScroll = () => {
-    if (!body || !body.style) return;
+    if (typeof document === 'undefined') return;
 
-    html.style.position = '';
-    html.style.overflow = '';
+    const { body, documentElement } = document;
+
+    documentElement.style.position = '';
+    documentElement.style.overflow = '';
     body.style.position = '';
     body.style.overflow = '';
     body.style.paddingRight = '';
