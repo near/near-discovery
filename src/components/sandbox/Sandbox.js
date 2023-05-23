dc652c20d724d4c333072a731edaf4086b3d3bc1
marcinbodnar <me@marcinbodnar.com>
2023-05-23 16:13:01 +0200

    Refactor condition

--- src/components/sandbox/Sandbox.js
+++ src/components/sandbox/Sandbox.js
@@ -455,11 +455,11 @@ export const Sandbox = ({ onboarding }) => {
 
   useEffect(() => {
     const pathArr = router.asPath.split('/');
-    if (pathArr[1] === 'onboarding' && pathArr[2] === 'start') {
+    if (onboarding && pathArr[2] === 'start') {
       setCurrentStep(0);
       router.replace('/onboarding');
     }
-  }, [router]);
+  }, [router, onboarding]);
 
   useEffect(() => {
     if (!defaultWidget || onboarding) {
