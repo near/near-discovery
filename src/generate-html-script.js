6ff8d7813910167095229ba55a93c90afe6c5196
Dmitriy <sheleg.dmitriy@gmail.com>
2023-06-06 00:02:22 +0300

    Create a script to generate html of page

---
+++ src/generate-html-script.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { generateHtml } = require("generate-html");
+
+// TODO: add an array of paths to generate html for
+generateHtml("dist", "https://beta.near.org");
