c8d1bbb87c255ddc3cfe311921dea4f95b9109a0
Dmitriy <sheleg.dmitriy@gmail.com>
2023-06-06 17:11:44 +0300

    Create subfolder for page

--- src/generate-html-script.js
+++ src/generate-html-script.js
@@ -1,5 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { generateHtml } = require("generate-html");
 
-// TODO: add an array of paths to generate html for
-generateHtml("dist", "https://beta.near.org");
+const getLastPartOfUrl = (url) => {
+  const parsedUrl = new URL(url);
+  const pathParts = parsedUrl.pathname.split('/');
+  return pathParts[pathParts.length - 1];
+}
+
+const urls = process.env.NEXT_PUBLIC_PAGE_URLS.split(", ");
+urls.forEach(async (url) => {
+  const subFolder = getLastPartOfUrl(url);
+  await generateHtml(`dist/${subFolder}`, url);
+});
