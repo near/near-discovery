cbac712458224c42a9910ae98bb5b658e1b07131
Dmitriy <34593263+shelegdmitriy@users.noreply.github.com>
2023-06-22 17:58:22 +0300

    Fix Zendesk location (#255)
    * Fix Zendesk location
    
    * Cleanup
    
    * Add default styled to be sure zendesk is styled

--- src/utils/zendesk.ts
+++ src/utils/zendesk.ts
@@ -3,19 +3,21 @@
   does not allow for custom styling.
 */
 
-export function styleZendesk() {
+export const styleZendesk = () => {
   const zwFrame = document.getElementById('launcher') as HTMLIFrameElement | null;
 
   if (!zwFrame || !zwFrame.contentDocument) return;
 
   const zwEmbed = zwFrame.contentDocument.getElementById('Embed') as HTMLElement | null;
-  const zwButton = zwEmbed?.getElementsByTagName('button')[0] as HTMLElement | null;
-  const zwIcon = zwEmbed?.getElementsByClassName('Icon')[0] as HTMLElement | null;
 
-  if (!zwEmbed || !zwButton || !zwIcon) return;
+  const zwButton = zwEmbed?.querySelector('[data-testid="launcher"]') as HTMLElement | null;
+  const zwIcon = zwButton?.querySelector('[data-testid="Icon"]') as HTMLElement | null;
+  const zwLabel = zwButton?.querySelector('[data-testid="launcher-label"]') as HTMLElement | null;
 
-  zwEmbed.style.opacity = '0.8';
-  zwButton.style.paddingRight = '1rem';
-  zwButton.style.paddingLeft = '1rem';
-  zwIcon.style.paddingRight = '0px';
+  if (!zwButton || !zwIcon || !zwLabel) return;
+
+  zwButton.style.opacity = "0.8";
+  zwButton.style.padding = ".92857rem";
+  zwIcon.style.paddingRight = "0";
+  zwLabel.remove();
 }
