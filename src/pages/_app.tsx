f5079ac15fe83a0b3850bdc5d22da49fe9ff06a5
Dmitriy <sheleg.dmitriy@gmail.com>
2023-06-22 14:35:09 +0300

    Hotfix: hide Zendesk widget

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,6 @@ import { useHashUrlBackwardsCompatibility } from '@/hooks/useHashUrlBackwardsCom
 import { usePageAnalytics } from '@/hooks/usePageAnalytics';
 import { init as initializeSegment } from '@/utils/analytics';
 import type { NextPageWithLayout } from '@/utils/types';
-import { styleZendesk } from '@/utils/zendesk';
 
 const VmInitializer = dynamic(() => import('../components/vm/VmInitializer'), {
   ssr: false,
@@ -37,25 +36,6 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     initializeSegment();
   }, []);
 
-  useEffect(() => {
-    const interval = setInterval(zendeskCheck, 20);
-
-    function zendeskCheck() {
-      //once the zendesk widget comes online, style it
-      const zwFrame = document.getElementById('launcher') as HTMLIFrameElement | null;
-      const zwEmbed = zwFrame?.contentDocument?.getElementById('Embed');
-      const zwButton = zwEmbed?.getElementsByTagName('button')[0];
-      if (zwButton) {
-        styleZendesk();
-        clearInterval(interval);
-      }
-    }
-
-    () => {
-      clearInterval(interval);
-    };
-  }, []);
-
   return (
     <>
       <Head>
@@ -64,40 +44,6 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
       </Head>
 
       <Script id="phosphor-icons" src="https://unpkg.com/@phosphor-icons/web@2.0.3" async />
-
-      <Script
-        src="https://static.zdassets.com/ekr/snippet.js?key=1736c8d0-1d86-4080-b622-12accfdb74ca"
-        id="ze-snippet"
-        async
-      />
-
-      <Script id="zendesk-config">
-        {`
-          window.zESettings = {
-            webWidget: {
-              color: { theme: '#2b2f31' },
-              offset: {
-                horizontal: '10px',
-                vertical: '10px',
-                mobile: { horizontal: '2px', vertical: '65px', from: 'right' },
-              },
-              contactForm: {
-                attachments: true,
-                title: { '*': 'Feedback and Support' },
-                fields: [
-                  {
-                    id: 13149356989591,
-                    prefill: { '*': localStorage.getItem('accountId') },
-                  },
-                ],
-              },
-              launcher: {
-                label: { '*': ' ' },
-              },
-            },
-          };
-        `}
-      </Script>
 
       <Script id="bootstrap" src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" />
 
