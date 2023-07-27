89a9ddfc1a671956a8f6eed3c7289db3b4c88bea
nature.xie <nature@ref.finance>
2023-07-27 17:21:29 +0800

    update menu

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,11 +1,8 @@
 import type { ReactNode } from 'react';
 import styled from 'styled-components';
 
-import { NearconBanner } from '../banners/NearconBanner';
-import { BosLoaderBanner } from '../BosLoaderBanner';
 import { DesktopNavigationLeft } from '../navigation/desktop/DesktopNavigationLeft';
 import { LoginBox } from '../navigation/desktop/LoginBox';
-import { Navigation } from '../navigation/Navigation';
 
 
 interface Props {
@@ -16,7 +13,8 @@ const Layout = styled.div`
   background: #1E202F;
   display:flex;
   .content{
-    padding:16px 60px;
+    position:relative;
+    padding:16px 36px;
     flex-grow:1;
   }
 `
