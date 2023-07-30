89a0fe33195c06daa046a20363a56b999efe6ebd
nature.xie <nature@ref.finance>
2023-07-30 13:31:15 +0800

    update left menu

--- src/components/navigation/desktop/DesktopNavigationLeft.tsx
+++ src/components/navigation/desktop/DesktopNavigationLeft.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 const Container = styled.div`
   position:relative;
