dad9f31cb4a4c297d7499b05da292fe92ddfd225
lq0-github <1441665200@qq.com>
2023-09-06 17:34:21 +0800

    Fix HomePage NearHomePagePowered width

--- src/components/near/NearHomePage/Near.HomePage.jsx
+++ src/components/near/NearHomePage/Near.HomePage.jsx
@@ -141,7 +141,7 @@ const Wrapper = styled.div`
   color: #fff;
   font-family: 'Gantari', sans-serif;
   overflow: hidden;
-  margin: 0 -32px;
+  margin: 0 -36px;
   padding: 0;
   @media (max-width: 900px) {
     margin: 0 -4px;
