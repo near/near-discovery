ccac0da7b448f859338784747172e3c482306790
李茜 <1441665200@qq.com>
2023-09-08 00:02:49 +0800

    Add near bridge tab mobile UI

--- src/components/rainbow-bridge/left-menu.tsx
+++ src/components/rainbow-bridge/left-menu.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
-import styled from 'styled-components';
-
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
+import React from 'react';
+import styled from 'styled-components';
 
 const MenuContainer = styled.div`
   padding: 128px 12px 35px 12px;
@@ -37,6 +35,37 @@ const MenuContainer = styled.div`
   }
   .icon {
     width: 26px;
+  }
+  @media (max-width:900px) {
+    margin: 0;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 16px;
+    background: #222436;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+    padding: 0 16px;
+    .item{
+      width: auto;
+      padding: 0;
+      height: 76px;
+      display: grid;
+      text-align: center;
+      align-items: center;
+      margin-bottom: 0;
+      span{
+        margin-left: 26px;
+        margin-bottom: -28px;
+      }
+    }
+    .item.active {
+    background-image: none;
+    color:  #E9F456;
+    border-color: transparent;
+  }
   }
 `;
 
