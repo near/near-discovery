7734c411a6677120c170248b276e4e7bc1ba916c
marcinbodnar <me@marcinbodnar.com>
2023-09-14 21:59:43 +0200

    Add install and ecticagte event listeners

---
+++ public/service-worker.js
@@ -0,0 +1,5 @@
+
+self.addEventListener('install', (event) => console.log('SW - installing in progress.', event));
+
+self.addEventListener('activate', (event) => console.log('SW - activateing in progress.', event));
+
