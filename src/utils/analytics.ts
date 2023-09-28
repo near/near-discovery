5a196ef212a5be56cc4c7ba14010afeddc012670
Roshaan Siddiqui <siddiqui.roshaan@gmail.com>
2023-09-28 12:07:45 -0500

    feat: gets componnet src + sends to databricks

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -101,11 +101,13 @@ export function recordPageView(pageName: string) {
 }
 
 const record = (eventType: string, e: UIEvent | PointerEvent) => {
+  console.log("component src:", getComponentName(e.target as HTMLElement))
   const key = get(e.target, 'placeholder', get(e.target, 'innerText', get(e.target, 'href')));
   recordEventWithProps(eventType, {
     element: truncate(key, { length: 255 }),
     url: e.target ? filterURL((e.target as HTMLElement).baseURI) : '',
     xPath: getXPath(e.target as HTMLElement),
+    componentSrc: getComponentName(e.target as HTMLElement),
   });
 };
 export const recordClick = (e: UIEvent | PointerEvent) => record('click', e);
@@ -156,6 +158,12 @@ export function recordEvent(eventLabel: string) {
   } catch (e) {
     console.error(e);
   }
+}
+
+function getComponentName(element: HTMLElement | null): string {
+  if (!element) return '';
+  if (element.hasAttribute('data-key')) return element.getAttribute('data-key') || '';
+  return ''
 }
 
 function getXPath(element: HTMLElement | null): string {
