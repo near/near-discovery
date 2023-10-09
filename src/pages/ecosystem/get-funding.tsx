8d6baed724b90714271eab345f0ae3bdaf0b0817
Caleb Jacob <thecalebjacob@gmail.com>
2023-10-09 14:53:22 -0600

    Use ecosystem BOS components for pages

--- src/pages/ecosystem/get-funding.tsx
+++ src/pages/ecosystem/get-funding.tsx
@@ -1,26 +1,20 @@
-import { useEffect } from 'react';
-
-import { MetaTags } from '@/components/MetaTags';
-import { NearOrgEcosystemGetFundingPage } from '@/components/near-org/NearOrg.Ecosystem.GetFundingPage';
+import { ComponentWrapperPage } from '@/components/near-org/ComponentWrapperPage';
+import { useBosComponents } from '@/hooks/useBosComponents';
 import { useDefaultLayout } from '@/hooks/useLayout';
-import { useCurrentComponentStore } from '@/stores/current-component';
 import type { NextPageWithLayout } from '@/utils/types';
 
 const EcosystemGetFundingPage: NextPageWithLayout = () => {
-  const setComponentSrc = useCurrentComponentStore((store) => store.setSrc);
-
-  useEffect(() => {
-    setComponentSrc(null);
-  }, [setComponentSrc]);
+  const components = useBosComponents();
 
   return (
-    <>
-      <MetaTags
-        title="NEAR | Get Funding"
-        description="Get funded while building on the Blockchain Operating System for an Open Web. The NEAR ecosystem offers multiple funding options to support initiatives aimed at decentralizing, growing, and innovating on NEAR."
-      />
-      <NearOrgEcosystemGetFundingPage />
-    </>
+    <ComponentWrapperPage
+      src={components.nearOrg.ecosystemGetFundingPage}
+      meta={{
+        title: 'NEAR | Get Funding',
+        description:
+          'Get funded while building on the Blockchain Operating System for an Open Web. The NEAR ecosystem offers multiple funding options to support initiatives aimed at decentralizing, growing, and innovating on NEAR.',
+      }}
+    />
   );
 };
 
--- src/pages/ecosystem/work-and-earn.tsx
+++ src/pages/ecosystem/work-and-earn.tsx
@@ -1,29 +1,23 @@
-import { useEffect } from 'react';
-
-import { MetaTags } from '@/components/MetaTags';
-import { NearOrgEcosystemWorkAndEarnPage } from '@/components/near-org/NearOrg.Ecosystem.WorkAndEarnPage';
+import { ComponentWrapperPage } from '@/components/near-org/ComponentWrapperPage';
+import { useBosComponents } from '@/hooks/useBosComponents';
 import { useDefaultLayout } from '@/hooks/useLayout';
-import { useCurrentComponentStore } from '@/stores/current-component';
 import type { NextPageWithLayout } from '@/utils/types';
 
-const EcosystemGetFundingPage: NextPageWithLayout = () => {
-  const setComponentSrc = useCurrentComponentStore((store) => store.setSrc);
-
-  useEffect(() => {
-    setComponentSrc(null);
-  }, [setComponentSrc]);
+const EcosystemWorkAndEarnPage: NextPageWithLayout = () => {
+  const components = useBosComponents();
 
   return (
-    <>
-      <MetaTags
-        title="NEAR | Work and Earn"
-        description="Work and Earn across the NEAR ecosystem. Find a job, get a grant for your project, and earn bounties."
-      />
-      <NearOrgEcosystemWorkAndEarnPage />
-    </>
+    <ComponentWrapperPage
+      src={components.nearOrg.ecosystemWorkAndEarnPage}
+      meta={{
+        title: 'NEAR | Work and Earn',
+        description:
+          'Work and Earn across the NEAR ecosystem. Find a job, get a grant for your project, and earn bounties.',
+      }}
+    />
   );
 };
 
-EcosystemGetFundingPage.getLayout = useDefaultLayout;
+EcosystemWorkAndEarnPage.getLayout = useDefaultLayout;
 
-export default EcosystemGetFundingPage;
+export default EcosystemWorkAndEarnPage;
