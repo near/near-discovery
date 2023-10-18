f643b872be96c9c6976ad08b3b37bda8253af633
Dmitriy <sheleg.dmitriy@gmail.com>
2023-10-19 00:01:53 +0300

    Update wallet-selector

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useEffect } from 'react';
+
 import { ComponentWrapperPage } from '@/components/near-org/ComponentWrapperPage';
 import { useBosComponents } from '@/hooks/useBosComponents';
 import { useDefaultLayout } from '@/hooks/useLayout';
@@ -7,14 +9,50 @@ import type { NextPageWithLayout } from '@/utils/types';
 const SettingsPage: NextPageWithLayout = () => {
   const components = useBosComponents();
   const idOS = useAuthStore((store) => store.idOS);
+  const near = useAuthStore((store) => store.vmNear);
 
   console.log('settings | idOS: ', idOS);
+
+  const connectIdOS = useCallback(async () => {
+    if (!idOS) {
+      return;
+    }
+    const wallet = await (await near.selector).wallet();
+    if (!wallet) {
+      return;
+    }
+    console.log('wallet: ', wallet);
+    try {
+      await idOS?.auth.setNearSigner(wallet); // a WalletSelector.Wallet
+      await idOS?.crypto.init();
+    } catch (error: any) {
+      console.error('Failed to set signer: ', error);
+    }
+  }, [idOS, near]);
+
+  const getUserInfo = useCallback(async () => {
+    if (!idOS) {
+      return;
+    }
+    try {
+      // const userInfo = await idOS.auth.currentUser();
+      const credentials = await idOS.data.list('credentials');
+      console.log('credentials: ', credentials);
+      return credentials;
+    } catch (error: any) {
+      console.error('Failed to get user info: ', error);
+    }
+  }, [idOS]);
+
+  useEffect(() => {
+    getUserInfo();
+  }, [getUserInfo]);
 
   return (
     <ComponentWrapperPage
       src={components.settings.index}
       meta={{ title: 'NEAR | Settings', description: '' }}
-      componentProps={{}}
+      componentProps={{ idOS: null, connectIdOS, getUserInfo }}
     />
   );
 };
