af96eb7e5b0ffcacd42482650a81afb1ff3f0596
max-mainnet <max@ref.finance>
2023-10-19 23:24:41 +0800

    optimize code

--- src/pages/uniswap.tsx
+++ src/pages/uniswap.tsx
@@ -9,6 +9,115 @@ import { useEthersProviderContext } from '@/data/web3';
 import { ethers } from 'ethers';
 import { useSetChain } from '@web3-onboard/react';
 
+const TopContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  .top-login {
+    display: flex;
+    .top-login-Source {
+      align-items: center;
+      background: rgba(45, 50, 71, 1);
+      border-radius: 12px;
+      padding: 10px 12px;
+      display: flex;
+      color: #fff;
+      margin-right: 20px;
+      font-size: 14px;
+      cursor: pointer;
+      img {
+        margin-right: 4px;
+      }
+    }
+    .top-login-select {
+      margin-right: 16px;
+      background: rgba(21, 22, 23, 1);
+      border-radius: 12px;
+      padding: 4px;
+      display: flex;
+      cursor: pointer;
+      position: relative;
+
+      .select-item-wrapper {
+        display: flex;
+        align-items: center;
+        gap: 6px;
+        cursor: pointer;
+      }
+      .selsect-item-img {
+        width: 32px;
+        height: 32px;
+        line-height: 32px;
+        text-align: center;
+        border-radius: 8px;
+        margin-right: 8px;
+      }
+      .selsect-item-icon {
+        margin-right: 12px;
+        height: 32px;
+        line-height: 32px;
+      }
+      .login-select-popup {
+        position: absolute;
+        top: 50px;
+        left: -178px;
+        background: rgba(45, 50, 71, 1);
+        border-radius: 12px;
+        padding: 12px;
+        width: 249px;
+        .select-popups-item {
+          width: 100%;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 8px;
+          border-radius: 8px;
+          .flex-grow {
+            flex-grow: 1;
+          }
+          .popup-item-img {
+            width: 32px;
+            height: 32px;
+            line-height: 32px;
+            text-align: center;
+            border-radius: 8px;
+            margin-right: 8px;
+          }
+          .popups-item-text {
+            font-size: 14px;
+            font-weight: 400;
+            color: #fff;
+          }
+        }
+        .select-popups-item:hover {
+          background: rgba(31, 35, 53, 0.5);
+        }
+        .selected {
+          background: rgba(31, 35, 53, 0.5);
+        }
+      }
+    }
+    .top-login-button {
+      font-family: Gantari;
+      font-size: 16px;
+      line-height: 19px;
+      color: #fff;
+      background: rgba(121, 79, 221, 1);
+      padding: 10px 16px;
+      border-radius: 16px;
+      cursor: pointer;
+    }
+  }
+`;
+
+const logoUrl = 'https://ipfs.near.social/ipfs/bafkreihjoszjyw5ebgi2uur7jbkykdowwixexlk5oy24y24mu2q3a45qkq';
+
+const narrowUrl = 'https://ipfs.near.social/ipfs/bafkreien4qagdjuudb6yj53wutsj4f6zfodrgv4ztftzjgkvcdtjnjk564';
+
+const sourceIcon = 'https://ipfs.near.social/ipfs/bafkreihz27oqiw4djztfrsvmjnxcv7zfgiztdqzlkpo5jdejxpt2nybu54';
+
+const checkMark = 'https://ipfs.near.social/ipfs/bafkreibrxtpffmzoe46yg3qbt3pivpukf5ne4zra73g6blxofkpowlmupm';
+
 const formateAddress = (address: string) => {
   if (address.indexOf('.near') > -1) return address;
 
@@ -82,7 +191,7 @@ const popupsData = [
   },
 ];
 
-const Uniswap: NextPageWithLayout = () => {
+const TopContentComponent = () => {
   const [isSelectItemClicked, setIsSelectItemClicked] = useState(false);
   const [selectedItem, setSelectedItem] = useState<number | null>(0);
 
@@ -149,116 +258,6 @@ const Uniswap: NextPageWithLayout = () => {
     setIsSelectItemClicked(false);
   };
 
-  const components = useBosComponents();
-
-  const TopContent = styled.div`
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    .top-login {
-      display: flex;
-      .top-login-Source {
-        align-items: center;
-        background: rgba(45, 50, 71, 1);
-        border-radius: 12px;
-        padding: 10px 12px;
-        display: flex;
-        color: #fff;
-        margin-right: 20px;
-        font-size: 14px;
-        cursor: pointer;
-        img {
-          margin-right: 4px;
-        }
-      }
-      .top-login-select {
-        margin-right: 16px;
-        background: rgba(21, 22, 23, 1);
-        border-radius: 12px;
-        padding: 4px;
-        display: flex;
-        cursor: pointer;
-        position: relative;
-
-        .select-item-wrapper {
-          display: flex;
-          align-items: center;
-          gap: 6px;
-          cursor: pointer;
-        }
-        .selsect-item-img {
-          width: 32px;
-          height: 32px;
-          line-height: 32px;
-          text-align: center;
-          border-radius: 8px;
-          margin-right: 8px;
-        }
-        .selsect-item-icon {
-          margin-right: 12px;
-          height: 32px;
-          line-height: 32px;
-        }
-        .login-select-popup {
-          position: absolute;
-          top: 50px;
-          left: -178px;
-          background: rgba(45, 50, 71, 1);
-          border-radius: 12px;
-          padding: 12px;
-          width: 249px;
-          .select-popups-item {
-            width: 100%;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 8px;
-            border-radius: 8px;
-            .flex-grow {
-              flex-grow: 1;
-            }
-            .popup-item-img {
-              width: 32px;
-              height: 32px;
-              line-height: 32px;
-              text-align: center;
-              border-radius: 8px;
-              margin-right: 8px;
-            }
-            .popups-item-text {
-              font-size: 14px;
-              font-weight: 400;
-              color: #fff;
-            }
-          }
-          .select-popups-item:hover{
-            background: rgba(31, 35, 53, 0.5);
-          }
-          .selected {
-            background: rgba(31, 35, 53, 0.5);
-          }
-        }
-      }
-      .top-login-button {
-        font-family: Gantari;
-        font-size: 16px;
-        line-height: 19px;
-        color: #fff;
-        background: rgba(121, 79, 221, 1);
-        padding: 10px 16px;
-        border-radius: 16px;
-        cursor: pointer;
-      }
-    }
-  `;
-  const logoUrl = 'https://ipfs.near.social/ipfs/bafkreihjoszjyw5ebgi2uur7jbkykdowwixexlk5oy24y24mu2q3a45qkq';
-
-  const narrowUrl = 'https://ipfs.near.social/ipfs/bafkreien4qagdjuudb6yj53wutsj4f6zfodrgv4ztftzjgkvcdtjnjk564';
-
-  const sourceIcon = 'https://ipfs.near.social/ipfs/bafkreihz27oqiw4djztfrsvmjnxcv7zfgiztdqzlkpo5jdejxpt2nybu54';
-
-  const checkMark = 'https://ipfs.near.social/ipfs/bafkreibrxtpffmzoe46yg3qbt3pivpukf5ne4zra73g6blxofkpowlmupm';
-
   useEffect(() => {
     if (connectedChain) {
       const findSelectedIndex = popupsData.findIndex((item) => item.chainId === eval(connectedChain.id));
@@ -270,82 +269,95 @@ const Uniswap: NextPageWithLayout = () => {
   }, [connectedChain]);
 
   return (
-    <>
-      <TopContent>
-        <div className="top-logo">
-          <img src={logoUrl} alt="" />
+    <TopContent>
+      <div className="top-logo">
+        <img src={logoUrl} alt="" />
+      </div>
+      <div className="top-login">
+        <div
+          className="top-login-Source"
+          onClick={() => {
+            window.open(
+              'https://near.org/near/widget/ComponentDetailsPage?src=dapdapbos.near/widget/Uniswap.Swap.Dex&tab=source',
+              '_blank',
+            );
+          }}
+        >
+          <img src={sourceIcon} alt="" />
+          View Code
         </div>
-        <div className="top-login">
-          <div
-            className="top-login-Source"
-            onClick={() => {
-              window.open(
-                'https://near.org/near/widget/ComponentDetailsPage?src=dapdapbos.near/widget/Uniswap.Swap.Dex&tab=source',
-                '_blank',
-              );
-            }}
-          >
-            <img src={sourceIcon} alt="" />
-            View Code
-          </div>
-          <div className="top-login-select">
-            <div className="select-item-wrapper" onClick={handleSelectItemClick}>
-              <div
-                className="selsect-item-img"
-                style={{
-                  backgroundColor: selectedItem !== null ? popupsData[selectedItem].bgColor : 'transparent',
-                }}
-              >
-                {selectedItem !== null && <img src={popupsData[selectedItem].icon} alt="" />}
-              </div>
-              <div className="selsect-item-icon">
-                <img
-                  style={{ transform: isSelectItemClicked ? 'rotate(180deg)' : 'rotate(0deg)' }}
-                  src={narrowUrl}
-                  alt=""
-                />
-              </div>
+        <div className="top-login-select">
+          <div className="select-item-wrapper" onClick={handleSelectItemClick}>
+            <div
+              className="selsect-item-img"
+              style={{
+                backgroundColor: selectedItem !== null ? popupsData[selectedItem].bgColor : 'transparent',
+              }}
+            >
+              {selectedItem !== null && <img src={popupsData[selectedItem].icon} alt="" />}
             </div>
+            <div className="selsect-item-icon">
+              <img
+                style={{ transform: isSelectItemClicked ? 'rotate(180deg)' : 'rotate(0deg)' }}
+                src={narrowUrl}
+                alt=""
+              />
+            </div>
+          </div>
 
-            {isSelectItemClicked && (
-              <div className="login-select-popup">
-                {popupsData.map((item, index) => (
-                  <div
-                    className={`select-popups-item ${selectedItem === index ? 'selected' : ''}`}
-                    key={index}
-                    onClick={() => handleItemClick({ index, chainId: item.chainId })}
-                  >
-                    <div className="popup-item-img" style={{ backgroundColor: item.bgColor }}>
-                      <img src={item.icon} alt="" />
-                    </div>
-                    <div className="popups-item-text">{item.text}</div>
-                    <div className="flex-grow"></div>
-                    {selectedItem === index && (
-                      <div className="check-mark">
-                        <img src={checkMark} alt="check-mark" />
-                      </div>
-                    )}
+          {isSelectItemClicked && (
+            <div className="login-select-popup">
+              {popupsData.map((item, index) => (
+                <div
+                  className={`select-popups-item ${selectedItem === index ? 'selected' : ''}`}
+                  key={index}
+                  onClick={() => handleItemClick({ index, chainId: item.chainId })}
+                >
+                  <div className="popup-item-img" style={{ backgroundColor: item.bgColor }}>
+                    <img src={item.icon} alt="" />
                   </div>
-                ))}
-              </div>
-            )}
-          </div>
-          <div
-            className="top-login-button"
-            onClick={() => {
-              connect();
-            }}
-          >
-            {sender ? formateAddress(sender) : 'Connect'}
-          </div>
+                  <div className="popups-item-text">{item.text}</div>
+                  <div className="flex-grow"></div>
+                  {selectedItem === index && (
+                    <div className="check-mark">
+                      <img src={checkMark} alt="check-mark" />
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-      </TopContent>
+        <div
+          className="top-login-button"
+          onClick={() => {
+            connect();
+          }}
+        >
+          {sender ? formateAddress(sender) : 'Connect'}
+        </div>
+      </div>
+    </TopContent>
+  );
+};
+const Uniswap: NextPageWithLayout = () => {
+  const [
+    {
+      chains, // the list of chains that web3-onboard was initialized with
+      connectedChain, // the current chain the user's wallet is connected to
+      settingChain, // boolean indicating if the chain is in the process of being set
+    },
+    setChain, // function to call to initiate user to switch chains in their wallet
+  ] = useSetChain();
 
+  return (
+    <>
+      <TopContentComponent />
       <ComponentWrapperPage
         componentProps={{
           chainId: connectedChain ? eval(connectedChain.id) : 1,
         }}
-        src={components.uniswap || ''}
+        src={'dapdapbos.near/widget/Uniswap.Swap.Dex'}
       />
     </>
   );
