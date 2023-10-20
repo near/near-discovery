11101a78dd7b494c4246fd1c74cdc230fcfd9752
Charles Garrett <charleslavong@gmail.com>
2023-10-20 16:11:23 -0400

    Revert "feat: lauchdarkly support for server-side feature flag resolution"
    This reverts commit 540a0c740b9668c19d188b12633d370575f44b1b.

--- src/pages/[componentAccountId]/widget/[componentName].tsx
+++ src/pages/[componentAccountId]/widget/[componentName].tsx
@@ -11,7 +11,6 @@ import { useDefaultLayout } from '@/hooks/useLayout';
 import { useAuthStore } from '@/stores/auth';
 import { useCurrentComponentStore } from '@/stores/current-component';
 import type { NextPageWithLayout } from '@/utils/types';
-import { getClient } from '../../../../ldserver';
 
 type ComponentMetaPreview = {
   title: string;
@@ -73,18 +72,11 @@ export const getServerSideProps: GetServerSideProps<{
     };
   }
 
-  const client = await getClient();
-  const flags = await client.allFlagsState({
-    kind: 'machine',
-    key: 'vercel-for-static-deferred-pre-rendering',
-  });
-
   const meta = await fetchPreviewData(componentAccountId, componentName);
 
   return {
     props: {
       meta,
-      featureFlags: flags.toJSON(),
     },
   };
 };
