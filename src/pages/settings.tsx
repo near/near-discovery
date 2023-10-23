5e26e89fb25b3fa0d846b6a50ecca25ea5da2505
Charles Garrett <charleslavong@gmail.com>
2023-10-23 17:46:40 -0400

    chore: update this settings example to show a potential way to support client side feature flags leveraging local storage

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,44 +1,94 @@
 import { useDefaultLayout } from '@/hooks/useLayout';
+import { VmComponent } from '@/components/vm/VmComponent';
 import type { NextPageWithLayout } from '@/utils/types';
-import { ComponentWrapperPage } from '@/components/near-org/ComponentWrapperPage';
 import { useBosComponents } from '@/hooks/useBosComponents';
-import { useEffect } from 'react';
+import { ComponentWrapperPage } from '@/components/near-org/ComponentWrapperPage';
+import { useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { getClient } from '../../ldserver';
+import { isLocalStorageSupported } from '@/utils/notificationsHelpers';
 
-export const getStaticProps = async () => {
-  const client = await getClient();
-  const flags = await client.allFlagsState({
-    kind: 'machine',
-    key: 'vercel-for-static-deferred-pre-rendering',
-  });
-
-  return {
-    props: {
-      featureFlags: flags.toJSON(),
-    },
-  };
-};
-
-const SettingsPage: NextPageWithLayout = ({ featureFlags }) => {
+const SettingsPage: NextPageWithLayout = () => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  const [redirect, setRedirect] = useState(false);
   const components = useBosComponents();
-  const { push } = useRouter();
+  let betaFeatures: any;
+  let accountId: any;
+  const router = useRouter();
 
-  console.log('featureFlags ', featureFlags);
+  if (isLocalStorageSupported()) {
+    betaFeatures = JSON.parse(localStorage.getItem('beta_features') || '{}');
+    accountId = localStorage.getItem('accountId');
+    console.log('beta features', betaFeatures);
+  }
+
   useEffect(() => {
-    if (!featureFlags.settings_beta_enabled) {
-      push('/');
+    if (!betaFeatures[accountId] || (betaFeatures[accountId] && !betaFeatures[accountId].settings_ui_enabled)) {
+      setDialogIsOpen(true);
     }
-  }, [featureFlags]);
+  }, [accountId]);
+
+  const handleCancelFunction = useCallback(() => {
+    setRedirect(true);
+    setTimeout(() => {
+      router.push('/');
+    }, 2500);
+  }, []);
+
+  const handleConfirmFunction = useCallback(() => {
+    localStorage.setItem(
+      'beta_features',
+      JSON.stringify({
+        ...(betaFeatures || {}),
+        [accountId]: {
+          ...(betaFeatures || {})[accountId],
+          settings_ui_enabled: true,
+        },
+      }),
+    );
+  }, []);
 
   return (
-    <ComponentWrapperPage
-      src={components.nearOrg.ecosystemPage}
-      meta={{
-        title: 'NEAR.org | Settings',
-        description: 'WIP Settings Area.',
-      }}
-    />
+    <>
+      {redirect && (
+        <VmComponent
+          src="near/widget/DIG.Dialog"
+          props={{
+            type: 'dialog',
+            title: 'Redirecting...',
+            description: "Ok, let's get you back home.",
+            confirmButton: false,
+            cancelButton: false,
+            open,
+          }}
+        />
+      )}
+      <VmComponent
+        src="near/widget/DIG.Dialog"
+        props={{
+          type: 'alert',
+          title: 'Join the Beta program for Personalized Settings?',
+          description: (
+            <span>
+              <p>Your data is yours to manage. </p>Get early access to advanced data management and customization
+              features, and help us make them better prior to a general public release.
+            </span>
+          ),
+          onCancel: handleCancelFunction,
+          onConfirm: handleConfirmFunction,
+          cancelButtonText: 'Cancel',
+          confirmButtonText: 'Confirm',
+          open: dialogIsOpen,
+          onOpenChange: (value: boolean) => setDialogIsOpen(value),
+        }}
+      />
+      <ComponentWrapperPage
+        src={components.nearOrg.ecosystemPage}
+        meta={{
+          title: 'NEAR.org | Settings',
+          description: 'WIP Settings Area.',
+        }}
+      />
+    </>
   );
 };
 
