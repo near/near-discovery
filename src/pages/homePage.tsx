f88255e5cf9beb12c0c79c272f4856a1d3d92662
lq0-github <1441665200@qq.com>
2023-10-26 18:41:23 +0800

    Update new home page tab

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -5,7 +5,7 @@ import { menuData } from '@/data/menuData';
 import { useBosComponents } from '@/hooks/useBosComponents';
 import { useDefaultLayout } from '@/hooks/useLayout';
 import type { NextPageWithLayout } from '@/utils/types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const NewHomePage: NextPageWithLayout = () => {
   const components = useBosComponents();
@@ -15,6 +15,25 @@ const NewHomePage: NextPageWithLayout = () => {
   const docsIcon = 'https://ipfs.near.social/ipfs/bafkreiae3ujempvwh2mowlpqoitmz7ohcawkrx2u26cezeit443ni475oe';
   const teleIcon = 'https://ipfs.near.social/ipfs/bafkreihsnpk5thlrjlk3jakd6vximvtdcqknsikhdu7fgpkw2jusmjptgq';
   const HelpIcon = 'https://ipfs.near.social/ipfs/bafkreighqgtfiphvdd32lg3io3k447ola32na3ovjppdrhtvfurccbekke';
+
+  const SearchIcon = (
+    <img src="https://ipfs.near.social/ipfs/bafkreih4njnef5mt7zzwx3l42lhkvw53aanyaxp24hvmiqv6m37fosfsim" alt="" />
+  );
+  const gridIcon = (
+    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="5.60005" height="5.60005" rx="1" fill="currentColor" />
+      <rect y="6.3999" width="5.60005" height="5.60005" rx="1" fill="currentColor" />
+      <rect x="6.39976" width="5.60005" height="5.60005" rx="1" fill="currentColor" />
+      <rect x="6.39976" y="6.3999" width="5.60005" height="5.60005" rx="1" fill="currentColor" />
+    </svg>
+  );
+  const tableIcon = (
+    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="12" height="2.4" rx="1.2" fill="currentColor" />
+      <rect y="4.7998" width="12" height="2.4" rx="1.2" fill="currentColor" />
+      <rect y="9.59961" width="12" height="2.4" rx="1.2" fill="currentColor" />
+    </svg>
+  );
 
   const NewHomePage = styled.div`
     color: #979abe;
@@ -130,19 +149,122 @@ const NewHomePage: NextPageWithLayout = () => {
     font-size: 16px;
     margin-bottom: 18px;
   `;
-  const NewHomePageRight = styled.div``;
-
-  const [selectedMenu, setSelectedMenu] = useState('');
+  const NewHomePageRight = styled.div`
+    margin-left: 40px;
+    width: 80%;
+    .page-right-content {
+      width: 80%;
+      position: relative;
+      .page-right-tab {
+        border: 1px solid rgba(52, 56, 56, 1);
+        background-color: #151515;
+        padding: 5px;
+        border-radius: 32px;
+        color: rgba(255, 255, 255, 1);
+        display: flex;
+        width: fit-content;
+        margin-bottom: 30px;
+        .right-tab-item {
+          font-size: 20px;
+          font-weight: 700;
+          padding: 8px 24px;
+          cursor: pointer;
+          border-radius: 32px;
+        }
+        .active {
+          background: #ebf479;
+          color: #000000;
+        }
+      }
+      .page-right-switch {
+        position: absolute;
+        right: 0;
+        top: 0;
+        border: 1px solid #5e617e;
+        padding: 4px;
+        display: flex;
+        border-radius: 8px;
+        .right-switch-item {
+          font-size: 12px;
+          color: #5e617e;
+          border-radius: 6px;
+          width: 24px;
+          height: 24px;
+          line-height: 24px;
+          text-align: center;
+          align-items: center;
+        }
+        .active {
+          background: #5e617e;
+          color: #000000;
+        }
+      }
+    }
+  `;
+  const Search = styled.div`
+    margin-bottom: 26px;
+    input {
+      width: 80%;
+      height: 48px;
+      line-height: 48px;
+      background: transparent;
+      border: 1px #373a53 solid;
+      padding-right: 24px;
+      border-radius: 12px;
+      padding: 16px;
+    }
+    input:focus {
+      outline: none;
+      color: #ffffff;
+      border: 1px solid rgba(235, 244, 121, 0.3);
+      background: #0d0e12;
+      box-shadow: none;
+    }
+    img {
+      margin-left: -36px;
+      cursor: pointer;
+    }
+  `;
+  const [selectedMenu, setSelectedMenu] = useState(() => {
+    if (typeof window !== 'undefined') {
+      return window.localStorage.getItem('selectedMenu') || '';
+    }
+    return '';
+  });
 
   const handleMenuClick = (path: string) => {
     setSelectedMenu(path);
   };
 
-  const [selectedMedalMenu, setSelectedMedalMenu] = useState('');
+  const [selectedMedalMenu, setSelectedMedalMenu] = useState(() => {
+    if (typeof window !== 'undefined') {
+      return window.localStorage.getItem('selectedMedalMenu') || '';
+    }
+    return '';
+  });
 
   const handleMedalMenuClick = (path: string) => {
     setSelectedMedalMenu(path);
   };
+
+  const [selectedTab, setSelectedTab] = useState(() => {
+    if (typeof window !== 'undefined') {
+      return window.localStorage.getItem('selectedTab') || '';
+    }
+    return '';
+  });
+
+  const handleTabClick = (path: string) => {
+    setSelectedTab(path);
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.localStorage.setItem('selectedMenu', selectedMenu);
+      window.localStorage.setItem('selectedMedalMenu', selectedMedalMenu);
+      window.localStorage.setItem('selectedTab', selectedTab);
+    }
+  }, [selectedMenu, selectedMedalMenu, selectedTab]);
 
   return (
     <NewHomePage>
@@ -211,7 +333,42 @@ const NewHomePage: NextPageWithLayout = () => {
           </div>
         </div>
       </NewHomePageLeft>
-      <NewHomePageRight></NewHomePageRight>
+      <NewHomePageRight>
+        <Search>
+          <input type="text" />
+          {SearchIcon}
+        </Search>
+        <div className="page-right-content">
+          <div className="page-right-tab">
+            <div
+              className={`right-tab-item ${selectedTab === 'TBD' ? 'active' : ''}`}
+              onClick={() => handleTabClick('TBD')}
+            >
+              Token-TBD🔥
+            </div>
+            <div
+              className={`right-tab-item ${selectedTab === 'token' ? 'active' : ''}`}
+              onClick={() => handleTabClick('token')}
+            >
+              Native token  
+            </div>
+          </div>
+          <div className="page-right-switch">
+            <div className="right-switch-item active">{gridIcon}</div>
+            <div className="right-switch-item">{tableIcon}</div>
+          </div>
+        </div>
+        {selectedTab == 'TBD' ? (
+          <>
+            <p>Token-TBD🔥</p>
+          </>
+        ) : null}
+        {selectedTab == 'token' ? (
+          <>
+            <p>Native token</p>
+          </>
+        ) : null}
+      </NewHomePageRight>
     </NewHomePage>
   );
 };
