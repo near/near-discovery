e636e3442c81235091de691932712cb394d0a1c2
Emily <emily@ref.finance>
2023-10-26 21:07:59 +0800

    Changed the optimisim tokens.

--- src/config/account/tokens/optimism.ts
+++ src/config/account/tokens/optimism.ts
@@ -3,7 +3,6 @@ import { optimism } from '@/config/tokens/optimism';
 
 export const optimismTokens = {
   native: optimism.eth,
-  '0x912CE59144191C1204E64559FE8253a0e49E6548': optimism.arb,
   '0x7F5c764cBc14f9669B88837ca1490cCa17c31607': optimism['usdc.e'],
   '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1': optimism.dai,
   '0xdFA46478F9e5EA86d57387849598dbFB2e964b02': optimism.mai,
