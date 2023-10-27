6e8b0ec030ff85d4cf9d0530ada85277dc65b9c0
Erdit Kurteshi <erdit.kurteshi@sqa-consulting.com>
2023-10-27 12:09:46 +0200

    added nearmobilewallet in wallet-selector

--- src/components/vm/VmInitializer.tsx
+++ src/components/vm/VmInitializer.tsx
@@ -6,6 +6,7 @@ import { setupMeteorWallet } from '@near-wallet-selector/meteor-wallet';
 import type { WalletSelectorModal } from '@near-wallet-selector/modal-ui';
 import { setupModal } from '@near-wallet-selector/modal-ui';
 import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
+import { setupNearMobileWallet } from '@near-wallet-selector/near-mobile-wallet';
 import { setupNearWallet } from '@near-wallet-selector/near-wallet';
 import { setupNeth } from '@near-wallet-selector/neth';
 import { setupNightly } from '@near-wallet-selector/nightly';
@@ -91,6 +92,7 @@ export default function VmInitializer() {
               networkId,
               signInContractId,
             }) as any, // TODO: Refactor setupKeypom() to TS
+            setupNearMobileWallet()
           ],
         }),
         customElements: {
