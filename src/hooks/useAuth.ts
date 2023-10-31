46eda76c76964cb5ff277b757e19a8b521b5abad
Emily <emily@ref.finance>
2023-10-31 11:29:41 +0800

    Fixed middleware.

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,26 +1,24 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { useSearchParams } from 'next/navigation';
 import useAccount from './useAccount';
 import { useEthersProviderContext } from '@/data/web3';
 import * as http from '@/utils/http';
 import { getAccessToken, insertedAccessKey, checkAddressIsInvited } from '@/apis';
-import { setCookie, deleteCookie } from 'cookies-next';
+import { setCookie, deleteCookie, getCookie } from 'cookies-next';
 
 const useAuth = () => {
   const { account } = useAccount();
-  const searchParams = useSearchParams();
   const { useConnectWallet } = useEthersProviderContext();
   const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
   const [logging, setLogging] = useState(false);
   const router = useRouter();
-
   const logout = async () => {
     wallet && (await disconnect(wallet));
     window.localStorage.setItem(http.AUTH_TOKENS, '{}');
     insertedAccessKey('');
+    deleteCookie('LOGIN_ACCOUNT');
     deleteCookie('AUTHED_ACCOUNT');
-    router.replace(`/login?source=/`);
+    router.replace(`/login?source=${router.pathname}`);
   };
 
   const login = useCallback(async () => {
@@ -29,24 +27,23 @@ const useAuth = () => {
     try {
       const checked = await checkAddressIsInvited(account);
       if (!checked) {
-        router.replace(`/invite-code?source=/`);
+        router.replace(`/invite-code?source=${router.pathname}`);
         return;
       }
       await getAccessToken(account);
       setLogging(false);
       setCookie('AUTHED_ACCOUNT', account);
-      router.replace(searchParams.get('source') || '/');
+      if (router.pathname === '/login' || router.pathname === '/invite-code')
+        router.replace((router.query?.source as string) || '/');
     } catch (error) {
       setLogging(false);
     }
   }, [account]);
 
   useEffect(() => {
-    if (account) {
+    if (account && !getCookie('LOGIN_ACCOUNT')) {
       setCookie('LOGIN_ACCOUNT', account);
       login();
-    } else {
-      deleteCookie('LOGIN_ACCOUNT');
     }
   }, [account]);
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -17,8 +17,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-    '/((?!uniswap|invite-code|login|_next/static|_next/image|favicon.ico|api|images|fonts).*)',
-    '/^((?!.near/widget).*)+$/',
-  ],
+  matcher: ['/((?!uniswap|invite-code|login|_next/static|_next/image|favicon.ico|api|images|fonts).*)'],
 };
