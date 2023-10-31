66c63de2fe15913294d5862aedfa760b961020e2
max-mainnet <max@ref.finance>
2023-10-31 14:09:32 +0800

    merge main into add-zkevm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -88,6 +88,13 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/logo.png" />
       </Head>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-TR5G968');`}
+      </Script>
 
       <Script id="phosphor-icons" src="https://unpkg.com/@phosphor-icons/web@2.0.3" async />
 
@@ -108,7 +115,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
         async
       />
 
-      <Script id="zendesk-config">
+      <Script id="zendesk-config" strategy="afterInteractive">
         {`
           window.zESettings = {
             webWidget: {
@@ -136,6 +143,15 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
       <Script id="bootstrap" src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" />
 
       <VmInitializer />
+
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TR5G968"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
 
       {getLayout(<Component {...pageProps} />)}
 
