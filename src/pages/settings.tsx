62f4250b79e45db216712f7fb31f1f15d49aad50
Dmitriy <sheleg.dmitriy@gmail.com>
2023-11-03 21:51:47 +0200

    Add redirect for users who don't have a fractal account

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -6,7 +6,7 @@ import { useBosComponents } from '@/hooks/useBosComponents';
 import { useDefaultLayout } from '@/hooks/useLayout';
 import { useAuthStore } from '@/stores/auth';
 import { useIdosStore } from '@/stores/idosStore';
-import type { IdosWalletInfo, NextPageWithLayout } from '@/utils/types';
+import type { NextPageWithLayout } from '@/utils/types';
 
 const SettingsPage: NextPageWithLayout = () => {
   const components = useBosComponents();
@@ -21,7 +21,6 @@ const SettingsPage: NextPageWithLayout = () => {
       return;
     }
     const wallet = await (await near.selector).wallet();
-    console.log('idOS init with selector: ', idOS);
     await new Promise<void>(async (resolve, reject) => {
       try {
         console.log('waiting for signer...');
@@ -50,8 +49,7 @@ const SettingsPage: NextPageWithLayout = () => {
       try {
         console.log('waiting for signature...');
         const credentials = await idOS.data.list('credentials');
-        const wallets = (await idOS.data.list('wallets')) as IdosWalletInfo[];
-        setIdosStore({ credentials, wallets });
+        setIdosStore({ credentials });
         resolve();
       } catch (error: any) {
         const errorMessage = error.message ? error.message : 'unknown';
@@ -68,38 +66,25 @@ const SettingsPage: NextPageWithLayout = () => {
 
   useEffect(() => {
     if (idosUser && !idosUser.humanId) {
-      // window.location.href = '/settings';
-      console.log('No user info found. Check if user has an fractal account.');
+      const idosPrototype = Object.getPrototypeOf(idOS);
+      const createAccountUrl = idosPrototype?.constructor?.profileProviders[0];
       openToast({
         type: 'INFO',
         title: 'No idOS profile found.',
-        description: 'Need an idOS profile?',
+        description: "Need an idOS profile? Now you'll be redirected to the idOS website to create one.",
       });
+
+      setTimeout(() => {
+        window.location.href = createAccountUrl;
+      }, 3000);
     }
-  }, [idosUser]);
+  }, [idosUser, idOS]);
 
   useEffect(() => {
     if (idosUser && idosUser.humanId && !idosCredentials) {
       collectUserInfo();
     }
   }, [collectUserInfo, idosCredentials, idosUser]);
-
-  // useEffect(() => {
-  //   if (idOS && near && near.accountId && !idosUser) {
-  //     connectIdOS();
-  //   }
-  // }, [connectIdOS, idOS, idosUser, near]);
-
-  console.log(
-    'idOS: ',
-    idOS,
-    'idosCurrentUser: ',
-    idosUser,
-    'idosCredentials: ',
-    idosCredentials,
-    'idosConnected: ',
-    idosUser?.humanId ?? false,
-  );
 
   return (
     <ComponentWrapperPage
