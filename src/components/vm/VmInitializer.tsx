1e423907b51045070f64b815d5b3790ab16ee4dc
erditkurteshiSQA <95851348+erditkurteshiSQA@users.noreply.github.com>
2023-11-20 17:32:17 +0100

    Added NearMobileWallet in wallet-selector (#777)
    * added nearmobilewallet in wallet-selector
    
    * fixed prettier issue
    
    * small change

--- src/components/vm/VmInitializer.tsx
+++ src/components/vm/VmInitializer.tsx
@@ -7,6 +7,7 @@ import { setupMeteorWallet } from '@near-wallet-selector/meteor-wallet';
 import type { WalletSelectorModal } from '@near-wallet-selector/modal-ui';
 import { setupModal } from '@near-wallet-selector/modal-ui';
 import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
+import { setupNearMobileWallet } from '@near-wallet-selector/near-mobile-wallet';
 import { setupNearWallet } from '@near-wallet-selector/near-wallet';
 import { setupNeth } from '@near-wallet-selector/neth';
 import { setupNightly } from '@near-wallet-selector/nightly';
@@ -95,6 +96,7 @@ export default function VmInitializer() {
               signInContractId,
             }) as any, // TODO: Refactor setupKeypom() to TS
             setupLedger(),
+            setupNearMobileWallet(),
           ],
         }),
         customElements: {
