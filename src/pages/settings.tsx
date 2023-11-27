55adf7b2ad93c966d1f0e3e4e66f7e512bc423b6
Dmitriy <34593263+shelegdmitriy@users.noreply.github.com>
2023-11-27 17:55:55 +0200

    refactor: Change idOS data storage (#836)

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,4 +1,6 @@
-import { useCallback, useEffect } from 'react';
+import type { WalletSelector } from '@near-wallet-selector/core';
+import { useRouter } from 'next/router';
+import { useCallback, useEffect, useState } from 'react';
 
 import { openToast } from '@/components/lib/Toast';
 import { ComponentWrapperPage } from '@/components/near-org/ComponentWrapperPage';
@@ -6,7 +8,7 @@ import { useBosComponents } from '@/hooks/useBosComponents';
 import { useDefaultLayout } from '@/hooks/useLayout';
 import { useAuthStore } from '@/stores/auth';
 import { useIdosStore } from '@/stores/idosStore';
-import type { NextPageWithLayout } from '@/utils/types';
+import type { IdosUser, NextPageWithLayout } from '@/utils/types';
 
 const SettingsPage: NextPageWithLayout = () => {
   const components = useBosComponents();
@@ -15,54 +17,53 @@ const SettingsPage: NextPageWithLayout = () => {
   const idosUser = useIdosStore((state) => state.currentUser);
   const idosCredentials = useIdosStore((state) => state.credentials);
   const setIdosStore = useIdosStore((state) => state.set);
+  const [error, setError] = useState<string | null>(null);
+  const [showTooltip, setShowTooltip] = useState(false);
   const createAccountUrl =
     'https://app.fractal.id/authorize?client_id=PXAbBxPErSPMXiKmMYQ3ged8Qxwqg1Px7ymhsuhaGP4&redirect_uri=https%3A%2F%2Fnear.org%2Fsettings&response_type=code&scope=contact%3Aread%20verification.uniqueness%3Aread%20verification.uniqueness.details%3Aread%20verification.idos%3Aread%20verification.idos.details%3Aread%20verification.wallet-near%3Aread%20verification.wallet-near.details%3Aread';
+  const router = useRouter();
 
   const connectIdOS = useCallback(async () => {
-    if (!near || !idOS) {
-      return;
-    }
-    const wallet = await (await near.selector).wallet();
-    await new Promise<void>(async (resolve, reject) => {
-      try {
-        const currentUser = (await idOS.setSigner('NEAR', wallet)) as any;
-        setIdosStore({ currentUser });
-        resolve();
-      } catch (error: any) {
-        console.error('Failed to init wallet + idOS: ', error);
-        const errorMessage = error.message ? error.message : 'unknown';
+    if (!near || !idOS) return;
+    const wallet = (await (await near.selector).wallet()) as WalletSelector['wallet'];
+    try {
+      const currentUser = idosUser ?? ((await idOS.setSigner('NEAR', wallet)) as unknown as IdosUser);
+      setIdosStore({ currentUser });
+    } catch (error: any) {
+      console.error('Failed to init wallet + idOS: ', error);
+      const errorMessage = error.message ? error.message : 'unknown';
+      setError(errorMessage);
+    } finally {
+      if ((idosUser && !idosUser.humanId) || (!idosCredentials && error)) {
         openToast({
           type: 'ERROR',
           title: 'Failed to init wallet + idOS:',
-          description: `${errorMessage}`,
+          description: `${error}`,
         });
-        reject();
       }
-    });
-  }, [idOS, near, setIdosStore]);
+    }
+  }, [error, idOS, idosCredentials, idosUser, near, setIdosStore]);
 
   const collectUserInfo = useCallback(async () => {
     if (!idOS) {
       return;
     }
 
-    await new Promise<void>(async (resolve, reject) => {
-      try {
-        const credentials = await idOS.data.list('credentials');
+    try {
+      if (!idosCredentials) {
+        const credentials = idosCredentials ?? (await idOS.data.list('credentials'));
         setIdosStore({ credentials });
-        resolve();
-      } catch (error: any) {
-        const errorMessage = error.message ? error.message : 'unknown';
-        console.error('Failed to get credentials: ', error);
-        openToast({
-          type: 'ERROR',
-          title: 'Failed to get credentials from IDOS',
-          description: `${errorMessage}`,
-        });
-        reject();
       }
-    });
-  }, [idOS, setIdosStore]);
+    } catch (error: any) {
+      const errorMessage = error.message ? error.message : 'unknown';
+      console.error('Failed to get credentials: ', error);
+      openToast({
+        type: 'ERROR',
+        title: 'Failed to get credentials from IDOS',
+        description: `${errorMessage}`,
+      });
+    }
+  }, [idOS, idosCredentials, setIdosStore]);
 
   useEffect(() => {
     if (idosUser && !idosUser.humanId) {
@@ -73,16 +74,22 @@ const SettingsPage: NextPageWithLayout = () => {
       });
 
       setTimeout(() => {
-        window.location.href = createAccountUrl;
+        router.push(createAccountUrl);
       }, 3000);
     }
-  }, [idosUser, idOS]);
+  }, [idosUser, idOS, router]);
 
   useEffect(() => {
     if (idosUser && idosUser.humanId && !idosCredentials) {
       collectUserInfo();
     }
   }, [collectUserInfo, idosCredentials, idosUser]);
+
+  useEffect(() => {
+    if (router && router.query && router.query.code) {
+      setShowTooltip(true);
+    }
+  }, [router]);
 
   return (
     <ComponentWrapperPage
@@ -92,6 +99,7 @@ const SettingsPage: NextPageWithLayout = () => {
         idosConnected: idosUser?.humanId ?? false,
         connectIdOS,
         idosCredentials,
+        showTooltip: showTooltip ?? false,
       }}
     />
   );
