5c92a2de830ac16a3675ca5994a2007f169bd962
Emily <emily@ref.finance>
2023-12-04 11:14:43 +0800

    Fixed 5514.

--- src/components/AccountSider/components/Chain.tsx
+++ src/components/AccountSider/components/Chain.tsx
@@ -31,12 +31,12 @@ const StyledChain = styled.div`
   line-height: 38px;
 `;
 const ChainLogo = styled.img`
-  width: 30px;
-  height: 30px;
+  width: 22px;
+  height: 22px;
   border-radius: 8px;
 `;
 const ChainName = styled.div`
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 400;
 `;
 const ArrowIconWrapper = styled.div`
@@ -45,22 +45,28 @@ const ArrowIconWrapper = styled.div`
 const ChainList = styled.div<{ display?: number }>`
   width: 204px;
   border: 1px solid #373a53;
-  border-radius: 8px;
-  background-color: rgba(55, 58, 83);
+  border-radius: 12px;
+  background-color: #303142;
   position: absolute;
   top: 38px;
   left: 0px;
   box-sizing: border-box;
   display: ${({ display }) => (display ? 'block' : 'none')};
   z-index: 200;
+  padding: 12px 0px;
 `;
 const ChainItem = styled(StyledChain)<{ active?: number }>`
   padding: 0px 10px;
+  display: flex;
+  alignitems: center;
+  justify-content: space-between;
   &:hover {
-    background-color: rgba(255, 255, 255, 0.1);
+    background-color: rgba(24, 26, 39, 0.3);
   }
-  ${({ active }) => active && 'background-color: #ebf479; pointer-events: none;color: #000'}
+  ${({ active }) => active && 'background-color: rgba(24, 26, 39, 0.3);pointer-events: none;'}
 `;
+
+const LogoName = styled(StyledChain)``;
 
 const Chain = ({
   mt,
@@ -90,7 +96,7 @@ const Chain = ({
   return (
     <StyledContainer
       $mt={mt}
-      $showName={showName ? 1: 0}
+      $showName={showName ? 1 : 0}
       onClick={(ev) => {
         ev.stopPropagation();
         showName ? setShowChains?.(!showChains) : setShowList(!showList);
@@ -113,10 +119,19 @@ const Chain = ({
             onClick={() => {
               switchNetwork(chain);
             }}
-            active={chain.chainId === currentChain?.chainId ? 1: 0}
+            active={chain.chainId === currentChain?.chainId ? 1 : 0}
           >
-            <ChainLogo src={chain.icon} />
-            <ChainName>{chain.chainName}</ChainName>
+            <LogoName>
+              <ChainLogo src={chain.icon} />
+              <ChainName>{chain.chainName}</ChainName>
+            </LogoName>
+            <div>
+              {chain.chainId === currentChain?.chainId && (
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="10" viewBox="0 0 14 10" fill="none">
+                  <path d="M1 4.11111L5.28571 8L13 1" stroke="#EBF479" stroke-width="2" stroke-linecap="round" />
+                </svg>
+              )}
+            </div>
           </ChainItem>
         ))}
       </ChainList>
