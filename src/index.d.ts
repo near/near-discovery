9a3af728fa4c3915521a4985d49bd777ca695b4f
Charles Garrett <charleslavong@gmail.com>
2024-01-26 13:56:50 -0500

    chore(data): send user agent detail with anonymous usage data
    as a workaround for chrome's default to reduced user-agent data

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,3 +7,12 @@ declare module 'nanoid' {
 interface Window {
   zE: (name: string, method: string) => void | undefined;
 }
+
+interface ExperimentalNaviagtor extends Navigator {
+  readonly userAgentData?: NavigatorUAData;
+}
+
+// https://wicg.github.io/ua-client-hints/#navigatoruadata
+interface NavigatorUAData extends UALowEntropyJSON {
+  getHighEntropyValues(hints: string[]): Promise<UADataValues>;
+}
