10c808054dcaa6554fdd14657ef0e01ac6db3161
Dmitriy <sheleg.dmitriy@gmail.com>
2024-02-14 20:35:23 +0100

    feat: create a new `papers` page

---
+++ src/pages/papers/index.tsx
@@ -0,0 +1,21 @@
+import { ComponentWrapperPage } from '@/components/near-org/ComponentWrapperPage';
+import { useBosComponents } from '@/hooks/useBosComponents';
+import { useDefaultLayout } from '@/hooks/useLayout';
+import type { NextPageWithLayout } from '@/utils/types';
+
+const PapersPage: NextPageWithLayout = () => {
+  const components = useBosComponents();
+  return (
+    <ComponentWrapperPage
+      src={components.nearOrg.papersPage}
+      meta={{
+        title: 'NEAR | Papers',
+        description: 'Join us as we dive deep into our technology.',
+      }}
+    />
+  );
+};
+
+PapersPage.getLayout = useDefaultLayout;
+
+export default PapersPage;
