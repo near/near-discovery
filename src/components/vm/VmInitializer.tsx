4226153dc759bca02ed679c1b75be084ceb8d11e
Ikeji Benjamin <benjaminikeji@gmail.com>
2024-02-15 18:11:33 +0100

    fix: remove unused function and import

--- src/components/vm/VmInitializer.tsx
+++ src/components/vm/VmInitializer.tsx
@@ -27,7 +27,6 @@ import {
   Widget,
 } from 'near-social-vm';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import React, { useCallback, useEffect, useState } from 'react';
 
 import { useEthersProviderContext } from '@/data/web3';
@@ -61,7 +60,6 @@ export default function VmInitializer() {
   const setVmStore = useVmStore((store) => store.set);
   const { requestAuthentication, saveCurrentUrl } = useSignInRedirect();
   const idOS = useIdOS();
-  const router = useRouter();
   const idosSDK = useIdosStore((state) => state.idOS);
 
   useEffect(() => {
@@ -144,14 +142,6 @@ export default function VmInitializer() {
       setWalletModal(selectorModal);
     });
   }, [idOS, near]);
-
-  const handleWalletSelectorMessage = (e: MessageEvent<{ showWalletSelector: boolean }>) => {
-    console.log('e.data ', e.data);
-    if (e.data.showWalletSelector) {
-      // Show wallet selector
-      walletModal?.show();
-    }
-  };
 
   const requestSignMessage = useCallback(
     async (message: string) => {
