e479193d4d232a1212b5bae0471c95354ca691ed
Ethan <starjs@qq.com>
2024-02-21 18:50:09 +0800

    fix: bitget coin68 & invite header change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -79,8 +79,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const _accountInit = debounce(accountInit, 500);
 
   useEffect(() => {
-    // if (!router.pathname.match(activityReg) || router.pathname === '/invite-code') {
-    if (router.pathname === '/invite-code') {
+    if (!router.pathname.match(activityReg) || router.pathname === '/invite-code') {
       _accountInit();
     } else {
       getInitialData();
