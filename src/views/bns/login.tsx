c07b691196176bee93f9267d476b26444a1146b8
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2024-02-22 11:25:01 +0800

    fix: fix video url

--- src/views/bns/login.tsx
+++ src/views/bns/login.tsx
@@ -18,7 +18,7 @@ const LoginView = () => {
       <StyledImage>
         {/* <Image src={loginBg} style={{ width: 824, height: 636 }} alt='loginBg' /> */}
         <StyledLoginVideo width={824} height={636} autoPlay muted playsInline loop>
-          <source src="/videos/login_background.mp4" type="video/mp4" />
+          <source src="https://s3.amazonaws.com/dapdap.prod/images/login_background.mp4" type="video/mp4" />
           <track
             src="/path/to/captions.vtt"
             kind="subtitles"
