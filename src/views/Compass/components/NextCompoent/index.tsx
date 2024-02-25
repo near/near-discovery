d63a7788e0e415a6c936a2cb5e3b9363ef5993e4
ran zhang <benjm240@gmail.com>
2024-02-25 22:12:12 +0800

    fix compass text bug

--- src/views/Compass/components/NextCompoent/index.tsx
+++ src/views/Compass/components/NextCompoent/index.tsx
@@ -24,7 +24,7 @@ const Title = styled.div`
 export default function Index() {
     return <NextWapper>
         <MidWapper>
-            <Title>DapDap Odyssey Vol. 2</Title>
+            <Title>{'DapDap Odyssey Vol. 2'}</Title>
             <Panel />
         </MidWapper>
     </NextWapper>
