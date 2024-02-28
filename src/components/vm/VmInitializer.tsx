a0d9727935f12489e9e5e870153a35f81d53960c
Osman Abdelnasir <esaminu1@icloud.com>
2024-02-29 00:17:29 +0400

    chore: remove preview walletUrl

--- src/components/vm/VmInitializer.tsx
+++ src/components/vm/VmInitializer.tsx
@@ -87,8 +87,7 @@ export default function VmInitializer() {
               relayerUrl:
                 networkId === 'testnet'
                   ? 'http://34.70.226.83:3030/relay'
-                  : 'https://near-relayer-mainnet.api.pagoda.co/relay',
-              walletUrl: 'https://fast-auth-signer-git-upgrade-impr-2ac97a-near-developer-console.vercel.app',
+                  : 'https://near-relayer-mainnet.api.pagoda.co/relay'
             }),
             setupKeypom({
               trialAccountSpecs: {
