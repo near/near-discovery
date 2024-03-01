ea4db2ae167f3ad18983e2d355507e8394af491b
Charles Garrett <charleslavong@gmail.com>
2024-03-01 10:00:57 -0500

    test: git submodule reference to static wp content. testing production builds and previews

--- src/pages/[...arbitrary].tsx
+++ src/pages/[...arbitrary].tsx
@@ -2,7 +2,6 @@
 // e.g. /about/contact-us, /papers/nightshade
 const extendableRoutes: Record<string, string> = {
   about: 'https://pages.near.org/about',
-  blog: 'https://pages.near.org/blog',
 };
 
 // routes which are extendable but may have some paths
