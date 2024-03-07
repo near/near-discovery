b3e045bbf4bce4dfad0780b527f4c947ac63878b
Charles Garrett <charleslavong@gmail.com>
2024-03-07 09:28:24 -0500

    perf: move this high bandwidth files to a cdn

--- next.config.js
+++ next.config.js
@@ -58,8 +58,13 @@ const nextConfig = {
         permanent: true,
       },
       {
+        source: '/files/:slug',
+        destination: '/papers/:slug',
+        permanent: true,
+      },
+      {
         source: '/papers/:slug',
-        destination: '/files/:slug.pdf',
+        destination: 'https://discovery-domain.org/papers/:slug',
         permanent: true,
       },
       {
@@ -102,7 +107,7 @@ const nextConfig = {
     {
       source: '/blog/:path*',
       destination: '/blog/:path*/index.html',
-    },
+    }
   ],
   headers: async () => [
     {
