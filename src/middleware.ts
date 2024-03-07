99efc8f5bf79cb3eedbb1072b3ade5d9b9431036
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2024-03-07 17:11:23 +0800

    fix: fix jump home

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,19 @@ import { NextResponse } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.next();
+  console.log('lol!!!')
+  const { pathname } = request.nextUrl
+  if (['/bns/guide', '/login', '/landing'].includes(pathname)) {
+    return NextResponse.redirect(new URL('/', request.url))
+  } else {
+    return NextResponse.next();
+  }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/((?!uniswap|login|_next/static|_next/image|favicon.ico|api|images|fonts|dapdap|videos).*)'],
+  matcher: [
+    '/',
+    '/((?!uniswap|_next/static|_next/image|favicon.ico|api|images|fonts|dapdap|videos).*)'
+  ],
 };
