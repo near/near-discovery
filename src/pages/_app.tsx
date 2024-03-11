fa15e1336c676321c90190eba8aecc35b667dd62
s-n-park <146785798+s-n-park@users.noreply.github.com>
2024-03-11 08:35:57 -0700

    style: apply pretty formatting to app.txs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -100,11 +100,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
       <Head>
         <meta name="google-site-verification" content="CDEVFlJTyVZ2vM7ePugKgWsl_7Rd-MrfDv42u0vZ0B0" />
         <link rel="icon" href="favicon.ico" />
-        <link
-          rel="canonical"
-          href={`${process.env.NEXT_PUBLIC_HOSTNAME}${router.asPath}`}
-          key="canonical"
-        />
+        <link rel="canonical" href={`${process.env.NEXT_PUBLIC_HOSTNAME}${router.asPath}`} key="canonical" />
         <link rel="manifest" href="manifest.json" />
       </Head>
 
