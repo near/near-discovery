6680176773d071bc5614e3a2e41b7323595e4f38
Charles Garrett <charleslavong@gmail.com>
2024-03-19 10:01:14 -0400

    chore: delete blog/index.html and fetch it's content serverside from nearorg_marketing github

--- next.config.js
+++ next.config.js
@@ -102,7 +102,7 @@ const nextConfig = {
     },
     {
       source: '/blog',
-      destination: '/blog/',
+      destination: '/blog/index.html',
     },
     {
       source: '/blog/:path*',
--- src/pages/blog/[...pages].tsx
+++ src/pages/blog/[...pages].tsx
@@ -1,25 +1,26 @@
 import type { GetServerSideProps } from 'next';
+import { useCallback, useEffect } from 'react';
 
 import { VmComponent } from '@/components/vm/VmComponent';
 import { useBosComponents } from '@/hooks/useBosComponents';
 import { useClearCurrentComponent } from '@/hooks/useClearCurrentComponent';
 import { useCookiePreferences } from '@/hooks/useCookiePreferences';
 import { useDefaultLayout } from '@/hooks/useLayout';
-import type { NextPageWithLayout } from '@/utils/types';
 import { recordHandledError } from '@/utils/analytics';
-import { useEffect, useCallback } from 'react';
-import { useRouter } from 'next/router';
+import type { NextPageWithLayout } from '@/utils/types';
 
 export const getServerSideProps = (async ({ resolvedUrl }) => {
-  const blog = resolvedUrl.split('blog/')[1];
-  const title = blog.substring(0, blog.indexOf('/'));
-
-  const res = await fetch(
-    `https://raw.githubusercontent.com/near/near-discovery/main/public/blog/${title}/index.html`,
-  ).catch((e) => {
-    recordHandledError({ title, message: 'failed to fetch github blog html for requested title' });
-    throw e;
-  });
+  const blogParts = resolvedUrl.split('blog/');
+  let title = 'index.html';
+  if (blogParts[1] !== title) {
+    title = `${blogParts[1].substring(0, blogParts[1].indexOf('/'))}/index.html`;
+  }
+  const res = await fetch(`https://raw.githubusercontent.com/near/near-discovery/main/public/blog/${title}`).catch(
+    (e) => {
+      recordHandledError({ title, message: 'failed to fetch github blog html for requested title' });
+      throw e;
+    },
+  );
 
   const __html = await (await res.blob()).text();
 
@@ -30,20 +31,18 @@ const StaticBlogPage: NextPageWithLayout = (props) => {
   useClearCurrentComponent();
   const cookieData = useCookiePreferences();
   const components = useBosComponents();
-  const router = useRouter();
 
-  const onBlogLinkClick = useCallback(
-    (event: any) => {
-      const url = event.target.href;
-      if (url) {
-        event.preventDefault();
-        router.push(url);
-      }
-    },
-    [router],
-  );
+  const onBlogLinkClick = useCallback((event: any) => {
+    const url = event.target.href;
+    if (url) {
+      event.preventDefault();
+      window.location = url;
+    }
+  }, []);
 
   useEffect(() => {
+    //this query fetches almost all links, except the clickable elements that are children of a tags
+    // like document.querySelectorAll("h3[class^='headline_type-4']")
     document.querySelectorAll("a[href^='/blog").forEach((element) => {
       element.addEventListener('click', onBlogLinkClick);
     });
