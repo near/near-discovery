17c365c0b2da475a2c7fd34e0c1e88103f585a02
Charles Garrett <charleslavong@gmail.com>
2024-03-19 17:23:06 -0400

    fix: ensure /blog is not served by [..arbitrary].tsx

--- src/pages/[...arbitrary].tsx
+++ src/pages/[...arbitrary].tsx
@@ -2,7 +2,6 @@
 // e.g. /about/contact-us, /papers/nightshade
 const extendableRoutes: Record<string, string> = {
   about: 'https://pages.near.org/about',
-  blog: '/blog/',
 };
 
 // routes which are extendable but may have some paths
