dad777db4fa8679e570e7acec55d225fed5a05e4
Caleb Jacob <thecalebjacob@gmail.com>
2024-04-05 15:12:39 -0600

    Fix ?sidebar=true test flag

--- src/components/sidebar-navigation/hooks.ts
+++ src/components/sidebar-navigation/hooks.ts
@@ -1,11 +1,23 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 import { sidebarLayoutEnabled as sidebarLayoutFeatureFlagEnabled } from '@/utils/config';
 
 export function useSidebarLayoutEnabled() {
   const router = useRouter();
-  const sidebarLayoutTestOverrideEnabled = router.query.sidebar === 'true';
+  const [sidebarLayoutTestOverrideEnabled, setSidebarLayoutTestOverrideEnabled] = useState(false);
   const sidebarLayoutEnabled = sidebarLayoutTestOverrideEnabled || sidebarLayoutFeatureFlagEnabled;
+
+  useEffect(() => {
+    /*
+      We only evaluate this once on page load so that we keep it enabled until they close
+      their browser tab.
+    */
+
+    setSidebarLayoutTestOverrideEnabled(router.query.sidebar === 'true');
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   /*
     The sidebarLayoutTestOverrideEnabled logic is only needed for short term testing.
