7dcecd1f99106b1eb7a693501327b46c1a68cb81
Charles Garrett <charleslavong@gmail.com>
2024-06-10 11:03:18 -0400

    chore: prevent google from indexing near.org content for the near.ai domain

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,4 +1,5 @@
 function generateSiteMap() {
+  if (process.env.NEXT_PUBLIC_HOSTNAME !== 'https://near.org') return null;
   //this fn serves to build a sitemap with the env specific hostname
   return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
