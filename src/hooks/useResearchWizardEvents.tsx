5c6990eb6b4b2f56c32b28b88083f770582ae0ad
jackson@near.org <jacksonnear.org@jacksonnearorgs-MacBook-Pro.local>
2024-07-11 12:32:11 -0700

    feat: complete send dismiss event, localstorage tracking, mobile view

---
+++ src/hooks/useResearchWizardEvents.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+
+export function useResearchFormEvents() {
+  const [isResearchFormDismissed, setIsResearchFormDismissed] = useState<boolean>();
+  const [showMobileResearchForm, setShowMobileResearchForm] = useState<boolean>(false);
+
+  useEffect(() => {
+    const isResearchFormDismissed = !!localStorage.getItem('researchFormDismissed') || false;
+    setIsResearchFormDismissed(isResearchFormDismissed);
+  }, []);
+
+  return { isResearchFormDismissed, showMobileResearchForm, setShowMobileResearchForm, setIsResearchFormDismissed };
+}
