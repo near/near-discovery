555e5bb57ae721ded7d7331fc9f6f81ca269d721
Matias Benary <matiasbenary@gmail.com>
2024-07-12 14:58:12 -0300

    feat: wip add search

--- src/components/sidebar-navigation/Search.tsx
+++ src/components/sidebar-navigation/Search.tsx
@@ -1,72 +1,10 @@
 import useDebounce from '@/hooks/useDebounce';
 import * as S from './styles';
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+import { useEffect, useRef, useState } from 'react';
 
-import styled from 'styled-components';
-
-const Card = styled.div`
-  background: white;
-  border-radius: 10px;
-  padding: 16px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  max-width: 400px;
-  margin-bottom: 8px;
-`;
-
-const Tile = styled.div`
-  display: flex;
-  width: 100%;
-`;
-const ImageContainer = styled.div`
-  width: 48px;
-  height: 48px;
-  border-radius: 50%;
-  overflow: hidden;
-  margin-right: 16px;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
-
-const Name = styled.h2`
-  margin: 0 0 8px;
-  font-size: 18px;
-  font-weight: bold;
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const Tagline = styled.p`
-  margin: 8px 0 0 8px;
-  font-size: 14px;
-  color: #666;
-`;
-
-const TagContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
-`;
-
-const Tag = styled.span`
-  background: #f0f0f0;
-  padding: 4px 8px;
-  border-radius: 16px;
-  font-size: 12px;
-  color: #333;
-  &:hover {
-    cursor: pointer;
-  }
-`;
+import { AppsResults } from './Search/AppsResults';
+import { ComponentsResults } from './Search/ComponentsResults';
+import { DocsResults } from './Search/DocsResults';
 
 const SEARCH_API_KEY_APPS = 'fc7644a5da5306311e8e9418c24fddc4';
 const APPLICATION_ID_APPS = 'B6PI9UKKJT';
@@ -100,104 +38,62 @@ const URLS = {
 };
 
 export const Search = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [showResults, setShowResults] = useState(false);
-  const [activeTab, setActiveTab] = useState('docs');
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [isFocus, setIsFocus] = useState<boolean>(false);
+  const [activeTab, setActiveTab] = useState<string>('docs');
   const [docs, setDocs] = useState([]);
   const [apps, setApps] = useState([]);
   const [components, setComponents] = useState([]);
   const debouncedSearchTerm = useDebounce<string>(searchTerm, 250);
-  const router = useRouter();
-
-  const redirect = (url: string) => () => router.push(url);
-  const tabs = {
-    docs: {
-      label: 'Docs',
-      component: docs,
-    },
-    apps: {
-      label: 'Apps',
-      component: apps,
-    },
-    components: {
-      label: 'Components',
-      component: components,
-    },
-  };
+  const componentRef = useRef(null);
 
   useEffect(() => {
     if (debouncedSearchTerm) {
       fetchResults();
-    } else {
-      setResults([]);
-      setShowResults(false);
     }
   }, [debouncedSearchTerm]);
 
-  const convertUrl = (url) => url.replace(/^https:\/\/docs\.near\.org\/(.+)$/, '/documentation/$1');
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (componentRef.current && !componentRef.current.contains(event.target)) {
+        setIsFocus(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   const docsComponents = (rawResp) => {
-    return rawResp.hits.map((item) => {
-      return (
-        // <Link href={convertUrl(item.url)}>
-        <S.CardDocs key={item.hierarchy.lvl0} onClick={redirect(convertUrl(item.url))}>
-          <S.TitleDocs>{item.hierarchy.lvl0}</S.TitleDocs>
-          <S.SubtitleDocs>{item.hierarchy.lvl1}</S.SubtitleDocs>
-          <S.ContentDocs>{item.content ? item.content.substr(0, 80) : ''}</S.ContentDocs>
-        </S.CardDocs>
-        // </Link>
-      );
+    return rawResp.hits.map((item, index) => {
+      return <DocsResults key={index} item={item}></DocsResults>;
     });
   };
 
   const appsComponents = (rawResp) => {
-    return Object.values(rawResp).map((item) => {
-      return (
-        // <Link href={`/nearcatalog.near/widget/Index?id=${item.slug}`} passHref>
-        <Card key={item.profile.name}>
-          <Tile>
-            <ImageContainer>
-              <Image src={item.profile.image.url} alt={item.profile.name} />
-            </ImageContainer>
-            <div>
-              <Name onClick={redirect(`/nearcatalog.near/widget/Index?id=${item.slug}`)}>{item.profile.name}</Name>
+    return Object.values(rawResp).map((item, index) => {
+      return <AppsResults key={index} item={item}></AppsResults>;
+    });
+  };
 
-              <TagContainer>
-                {Object.entries(item.profile.tags).map(([key, value]) => (
-                  <Tag key={key} onClick={redirect(`/nearcatalog.near/widget/Index?cat=${value}`)}>
-                    {value}
-                  </Tag>
-                ))}
-              </TagContainer>
-            </div>
-          </Tile>
-          <Tile>
-            <Tagline>{item.profile.tagline}</Tagline>
-          </Tile>
-        </Card>
-        // </Link>
-      );
+  const componentComponents = (rawResp) => {
+    return rawResp.hits.map((item, index) => {
+      return <ComponentsResults key={index} item={item}></ComponentsResults>;
     });
   };
 
   const fetchResults = async () => {
-    const docs = await fetchSearchHits('Docs', debouncedSearchTerm);
+    const [docs, apps, components] = await Promise.all([
+      fetchSearchHits('Docs', debouncedSearchTerm),
+      fetchCatalog(debouncedSearchTerm),
+      fetchSearchHits('Components', debouncedSearchTerm),
+    ]);
+
     setDocs(docsComponents(docs));
-    console.log('Search', docs);
-    const apps = await fetchCatalog(debouncedSearchTerm);
-    // console.log("fetchResults Apps", apps.values());
     setApps(appsComponents(apps));
-
-    const mockResults = [
-      `${activeTab} Resultado 1 para "${searchTerm}"`,
-      `${activeTab} Resultado 2 para "${searchTerm}"`,
-      `${activeTab} Resultado 3 para "${searchTerm}"`,
-    ];
-
-    // setApps(mockResults);
-    setComponents(mockResults);
-    setShowResults(true);
+    setComponents(componentComponents(components));
   };
 
   const handleSearch = (event) => {
@@ -208,24 +104,23 @@ export const Search = () => {
     setActiveTab(tabId);
   };
 
-  const handleClear = () => {
-    setSearchTerm('');
-    setShowResults(false);
+  const handleOnClick = () => {
+    setIsFocus(true);
   };
 
-  // const fetchSearchHits = (facet, query, { pageNumber, configs, optionalFilters }) => {
-  //   let body = {
-  //     query,
-  //     page: pageNumber ?? 0,
-  //     optionalFilters: optionalFilters ?? ["categories:nearcatalog<score=1>", "categories:widget<score=2>"],
-  //     clickAnalytics: true,
-  //     ...configs,
-  //   };
+  const handleClear = () => {
+    setSearchTerm('');
+    setDocs([]);
+    setApps([]);
+    setComponents([]);
+    setIsFocus(false);
+  };
 
   const fetchSearchHits = async (facet, query) => {
     const body = {
       query,
       page: 0,
+      optionalFilters: ['categories:nearcatalog<score=1>', 'categories:widget<score=2>'],
       clickAnalytics: true,
     };
 
@@ -249,25 +144,33 @@ export const Search = () => {
   };
 
   return (
-    <S.SearchWrapper>
-      <S.SearchIcon>🔍</S.SearchIcon>
-      <S.SearchInput type="text" placeholder="Buscar..." value={searchTerm} onChange={handleSearch} />
-      {searchTerm && <S.ClearButton onClick={handleClear}>×</S.ClearButton>}
+    <S.SearchWrapper ref={componentRef}>
+      <S.SearchContainer onClick={() => handleOnClick()} $isFocus={isFocus}>
+        <S.SearchIcon className="ph ph-magnifying-glass" $isFocus={isFocus} />
+        <S.SearchInput type="text" value={searchTerm} onChange={handleSearch} placeholder="Search..." />
+        {searchTerm && <i onClick={handleClear} className="ph ph-x-circle"></i>}
+      </S.SearchContainer>
 
-      <S.ResultsPopup $show={showResults}>
+      <S.ResultsPopup $show={isFocus}>
         <S.TabContainer>
-          <S.Tab $active={activeTab === 'docs'} onClick={() => handleTabChange('docs')}>
+          <S.Tab $active={activeTab === 'docs'} onClick={() => handleTabChange('docs')} $isFirst={true}>
             Docs
           </S.Tab>
           <S.Tab $active={activeTab === 'apps'} onClick={() => handleTabChange('apps')}>
             Apps
           </S.Tab>
-          <S.Tab $active={activeTab === 'components'} onClick={() => handleTabChange('components')}>
+          <S.Tab $active={activeTab === 'components'} onClick={() => handleTabChange('components')} $isLast={true}>
             Components
           </S.Tab>
         </S.TabContainer>
 
-        <S.ResultItem>{tabs[activeTab].component}</S.ResultItem>
+        <S.ResultItem>
+          {activeTab === 'docs' && docs}
+          {activeTab === 'apps' && apps}
+          {activeTab === 'components' && components}
+          {!(docs.length || apps.length || components.length) && !searchTerm && 'Type in to search'}
+          {!(docs.length || apps.length || components.length) && searchTerm && 'Searching...'}
+        </S.ResultItem>
       </S.ResultsPopup>
     </S.SearchWrapper>
   );
---
+++ src/components/sidebar-navigation/Search/ComponentsResults.tsx
@@ -0,0 +1,102 @@
+import { useRouter } from 'next/navigation';
+import styled from 'styled-components';
+
+const ListItem = styled.div`
+  background: white;
+  border-radius: 10px;
+  padding: 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  margin-bottom: 8px;
+`;
+
+const Logo = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 8px;
+  margin-right: 15px;
+  cursor: pointer;
+`;
+
+const WidgetName = styled.div`
+  font-weight: bold;
+  width: 120px;
+  text-overflow: ellipsis;
+  cursor: pointer;
+`;
+
+const Author = styled.div`
+  color: #888;
+  font-size: 0.9em;
+  width: 120px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  cursor: pointer;
+`;
+
+const Icon = styled.i`
+  cursor: pointer;
+`;
+interface Item {
+  author: string;
+  widget_name: string;
+  code: string;
+  receipt_hash: string;
+  receipt_date: number;
+  receipt_block_height: number;
+  indexed_date: number;
+  categories: string[];
+  image: {
+    ipfs_cid: string;
+  };
+  content: string;
+  name: string;
+  profile_name: string;
+  tags: string[];
+  objectID: string;
+  _snippetResult: {
+    content: {
+      value: string;
+      matchLevel: string;
+    };
+  };
+  _highlightResult: {
+    author: HighlightResult;
+    widget_name: HighlightResult;
+    content: HighlightResult;
+    name: HighlightResult;
+    tags: HighlightResult[];
+  };
+}
+
+interface HighlightResult {
+  value: string;
+  matchLevel: string;
+  fullyHighlighted?: boolean;
+  matchedWords: string[];
+}
+
+interface ComponentsResultsProps {
+  item: Item;
+}
+
+export const ComponentsResults = ({ item }) => {
+  const router = useRouter();
+  const redirect = (url: string) => () => router.push(url);
+  return (
+    <ListItem>
+      <Logo
+        onClick={redirect(`/near/widget/ComponentDetailsPage?src=${item.author}/widget/${item.widget_name}`)}
+        src={`https://ipfs.near.social/ipfs/${item?.image?.ipfs_cid}`}
+      ></Logo>
+      <WidgetName onClick={redirect(`/near/widget/ComponentDetailsPage?src=${item.author}/widget/${item.widget_name}`)}>
+        {item.name || item.profile_name}
+      </WidgetName>
+      <Author onClick={redirect(`/near/widget/ProfilePage?accountId=${item.author}`)}>@{item.author}</Author>
+      <Icon className="ph ph-arrow-right" onClick={redirect(`/${item.author}/widget/${item.widget_name}`)}></Icon>
+    </ListItem>
+  );
+};
