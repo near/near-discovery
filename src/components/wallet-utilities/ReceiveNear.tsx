b26ad30b72b25a3e51872a50a46446ef6bbd09ce
Caleb Jacob <thecalebjacob@gmail.com>
2024-08-01 14:37:27 -0600

    Wrap up wallet utilities, export fast auth account

--- src/components/wallet-utilities/ReceiveNear.tsx
+++ src/components/wallet-utilities/ReceiveNear.tsx
@@ -7,20 +7,24 @@ export const ReceiveNear = () => {
   const accountId = useAuthStore((store) => store.accountId);
 
   return (
-    <Flex align="center">
-      <Text style={{ marginRight: 'auto' }} weight={600} color="sand12">
-        {accountId}
-      </Text>
+    <Flex stack>
+      <Text size="text-s">Copy and share your account ID to receive NEAR:</Text>
 
-      <Tooltip content="Copy Account ID">
-        <Button
-          label="Copy Account ID"
-          icon={<Copy />}
-          variant="affirmative"
-          size="small"
-          onClick={() => copyTextToClipboard(accountId, accountId)}
-        />
-      </Tooltip>
+      <Flex align="center">
+        <Text style={{ marginRight: 'auto' }} weight={600} color="sand12">
+          {accountId}
+        </Text>
+
+        <Tooltip content="Copy Account ID">
+          <Button
+            label="Copy Account ID"
+            icon={<Copy />}
+            size="small"
+            fill="outline"
+            onClick={() => copyTextToClipboard(accountId, accountId)}
+          />
+        </Tooltip>
+      </Flex>
     </Flex>
   );
 };
