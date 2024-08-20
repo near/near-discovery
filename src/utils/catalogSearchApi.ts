c1e22a06d33e70f1c3460677e86309fbf6e018af
Matias Benary <matiasbenary@gmail.com>
2024-08-20 19:08:24 -0300

    fix: added error handling for catalogSearch

--- src/utils/catalogSearchApi.ts
+++ src/utils/catalogSearchApi.ts
@@ -1,4 +1,14 @@
 export const fetchCatalog = async (query: string) => {
-  const response = await fetch(`https://nearcatalog.xyz/wp-json/nearcatalog/v1/search?kw=${query}`);
-  return await response.json();
+  try {
+    const response = await fetch(`https://nearcatalog.xyz/wp-json/nearcatalog/v1/search?kw=${query}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching catalog:', error);
+    return { error: (error as Error).message };
+  }
 };
