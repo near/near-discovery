b2e87bcfe415f6ca1c0884618a8e3943cd7a4e7b
Robert Yan <mstr.raphael@gmail.com>
2024-08-31 18:03:48 +0800

    fix: import router query as props

--- src/pages/applications.tsx
+++ src/pages/applications.tsx
@@ -40,6 +40,7 @@ const ApplicationsPage: NextPageWithLayout = () => {
       src={components.applicationsPage}
       meta={{ title: 'NEAR | Applications', description: 'Featured applications built on NEAR' }}
       componentProps={{
+        targetProps: router.query,
         setURLSearchParams,
         scrollTo,
       }}
