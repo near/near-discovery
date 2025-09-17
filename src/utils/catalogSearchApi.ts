9a139ba580e2ef6786b11dbab1d7df1e5a018efa
Guillermo Alejandro Gallardo Diez <gagdiez@MacBookPro.home>
2025-09-17 18:56:50 +0200

    chore: redirect to docs.near.org

--- src/utils/catalogSearchApi.ts
+++
@@ -1,9 +0,0 @@
-export const fetchCatalog = async (query: string) => {
-  try {
-    const response = await fetch(`https://indexer.nearcatalog.xyz/wp-json/nearcatalog/v1/search?kw=${query}`);
-    return await response.json();
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-};
